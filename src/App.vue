5e12d723664591ecb1b801b78d357f83339ba114
Ben Walker <bwalke05@gmail.com>
2018-07-12 13:46:19 -0400

    Move style imports

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,3 @@
       router-link(to="/about") About
     router-view
 </template>
-
-<style lang="scss">
-@import "~buefy/lib/buefy.min";
-</style>
--- src/main.js
+++ src/main.js
@@ -4,6 +4,9 @@ import Buefy from 'buefy'
 import router from '@/config/router'
 import store from '@/config/store'
 
+import 'buefy/lib/buefy.min.css'
+import '@fortawesome/fontawesome-free/css/all.css'
+
 Vue.config.productionTip = false
 Vue.use(Buefy, { defaultIconPack: 'fas' })
 
