39986ee6d298d2fbc81bdc0aff831eb92485431c
Ben Walker <bwalke05@gmail.com>
2018-07-12 13:37:19 -0400

    Setup Buefy

--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Buefy from 'buefy'
 import router from '@/config/router'
 import store from '@/config/store'
 
 Vue.config.productionTip = false
+Vue.use(Buefy, { defaultIconPack: 'fas' })
 
 new Vue({
   router,
