0e0ef93955395bd973378a051bac163a12d56e47
Ben Walker <bwalke05@gmail.com>
2018-07-12 13:06:00 -0400

    Move router and store to config dir

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
-import router from './router'
-import store from './store'
+import router from '@/config/router'
+import store from '@/config/store'
 
 Vue.config.productionTip = false
 
