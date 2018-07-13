ca88ec6678e7a923a1074c359c9533142e356f54
Ben Walker <bwalke05@gmail.com>
2018-07-13 11:22:42 -0400

    Add signup component

---
+++ src/components/Signup.vue
@@ -0,0 +1,26 @@
+<template lang="pug">
+  #signup
+    .box
+      form
+        b-field(label="Username")
+          b-input(v-model.trim="username" type="text" rounded)
+        b-field(label="Email")
+          b-input(v-model.trim="email" type="text" rounded)
+        b-field(label="Password")
+          b-input(v-model="password" type="password" placeholder="••••••••" rounded password-reveal)
+        .has-text-right
+          button.button.is-primary(type="submit") Sign Up
+</template>
+
+<script>
+export default {
+  name: 'signup',
+  data () {
+    return {
+      username: '',
+      email: '',
+      password: ''
+    }
+  }
+}
+</script>
