1e701663c9205880b11ad80c6ff4989bcfac80e5
Ben Walker <bwalke05@gmail.com>
2018-07-13 10:07:10 -0400

    Adjust title size in navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -2,7 +2,7 @@
   #nav
     nav.navbar.has-shadow.is-spaced.is-transparent.is-primary
       .navbar-brand
-        h1.title.is-3.has-text-white {{ siteTitle }}
+        h1.title.is-2.has-text-white {{ siteTitle }}
       .navbar-menu
         .navbar-end
           router-link.navbar-item(to="/login" v-if="!authenticated") Login
