b3a059759dcd30a3fed89b86f6ea23906a7ac9dd
Ben Walker <bwalke05@gmail.com>
2018-07-13 16:20:40 -0400

    Set autofocus on first input when instance mounted

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -3,13 +3,13 @@
     .box
       form
         b-field(label="Username or Email")
-          b-input(v-model.trim="identifier" type="text" rounded)
+          b-input(v-model.trim="identifier" type="text" rounded ref="identifier")
         b-field(label="Password")
           b-input(v-model="password" type="password" placeholder="••••••••" rounded password-reveal)
         .field
           b-checkbox(v-model="rememberMe") Remember me
         .has-text-right
-          button.button.is-primary(type="submit") Log In
+          button.button.is-primary.is-outlined(type="submit") Log In
 </template>
 
 <script>
@@ -21,6 +21,9 @@ export default {
       password: '',
       rememberMe: false
     }
+  },
+  mounted () {
+    this.$refs.identifier.focus()
   }
 }
 </script>
