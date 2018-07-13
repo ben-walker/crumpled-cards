482c1846ef783b12f78fc7e6f7cd64b181224511
Ben Walker <bwalke05@gmail.com>
2018-07-13 09:54:36 -0400

    Add login/signup links to Navbar if not authenticated

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -3,13 +3,18 @@
     nav.navbar.has-shadow.is-spaced.is-transparent.is-primary
       .navbar-brand
         h1.title.is-3.has-text-white {{ siteTitle }}
+      .navbar-menu
+        .navbar-end
+          router-link.navbar-item(to="/login" v-if="!authenticated") Login
+          router-link.navbar-item(to="/signup" v-if="!authenticated") Signup
 </template>
 
 <script>
 export default {
   name: 'navbar',
   props: [
-    'siteTitle'
+    'siteTitle',
+    'authenticated'
   ]
 }
 </script>
