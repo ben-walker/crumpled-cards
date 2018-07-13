cb852c24f3d90fd43053c46fc4bf9c7f43e53dfb
Ben Walker <bwalke05@gmail.com>
2018-07-13 16:12:09 -0400

    Auth view for signing up/logging in

---
+++ src/views/Auth.vue
@@ -0,0 +1,20 @@
+<template lang="pug">
+  #auth
+    Navbar(siteTitle="Poker Nook" :authenticated="false")
+    section.section.is-medium
+      .columns.is-centered.is-mobile
+        .column.is-one-third
+          transition(name="fade" mode="out-in")
+            router-view
+</template>
+
+<script>
+import Navbar from '@/components/Navbar.vue'
+
+export default {
+  name: 'auth',
+  components: {
+    Navbar
+  }
+}
+</script>
