96e8166297f3dd0242f70ba88067b5d964bed339
Ben Walker <bwalke05@gmail.com>
2018-07-13 11:13:24 -0400

    Component for logging in

---
+++ src/components/Login.vue
@@ -0,0 +1,26 @@
+<template lang="pug">
+  #login
+    .box
+      form
+        b-field(label="Username or Email")
+          b-input(v-model.trim="identifier" type="text" rounded)
+        b-field(label="Password")
+          b-input(v-model="password" type="password" placeholder="••••••••" rounded password-reveal)
+        .field
+          b-checkbox(v-model="rememberMe") Remember me
+        .has-text-right
+          button.button.is-primary(type="submit") Log In
+</template>
+
+<script>
+export default {
+  name: 'login',
+  data () {
+    return {
+      identifier: '',
+      password: '',
+      rememberMe: false
+    }
+  }
+}
+</script>
