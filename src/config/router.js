03f47c41c7e279137232896e41fdc8aed8995d25
Ben Walker <bwalke05@gmail.com>
2018-07-13 14:07:53 -0400

    Add login/signup components to router

--- src/config/router.js
+++ src/config/router.js
@@ -1,12 +1,15 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 
+import Login from '@/components/Login.vue'
+import Signup from '@/components/Signup.vue'
+
 Vue.use(Router)
 
 const routes = [
   { path: '/', name: 'home' },
-  { path: '/login', name: 'login' },
-  { path: '/signup', name: 'signup' },
+  { path: '/login', name: 'login', component: Login },
+  { path: '/signup', name: 'signup', component: Signup },
   { path: '*', redirect: { name: 'home' } }
 ]
 
