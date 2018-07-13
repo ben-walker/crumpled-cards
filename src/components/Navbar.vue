00148dece24e50cc0f6db2854e6ce4c31f6ff6be
Ben Walker <bwalke05@gmail.com>
2018-07-13 09:34:08 -0400

    Add a navbar component

---
+++ src/components/Navbar.vue
@@ -0,0 +1,15 @@
+<template lang="pug">
+  #nav
+    nav.navbar.has-shadow.is-spaced.is-transparent.is-primary
+      .navbar-brand
+        h1.title.is-3.has-text-white {{ siteTitle }}
+</template>
+
+<script>
+export default {
+  name: 'navbar',
+  props: [
+    'siteTitle'
+  ]  
+}
+</script>
