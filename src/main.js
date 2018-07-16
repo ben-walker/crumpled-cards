ee63cb6d4518c1769ccce6064d5c1a89386cda6c
Ben Walker <bwalke05@gmail.com>
2018-07-16 16:35:43 -0400

    Remove shims config file, setup promise.prototype.finally

--- src/main.js
+++ src/main.js
@@ -6,6 +6,7 @@ import Buefy from 'buefy'
 import SimpleVueValidation from 'simple-vue-validator'
 import router from '@/config/router'
 import store from '@/config/store'
+import promiseFinally from 'promise.prototype.finally'
 
 import 'buefy/lib/buefy.min.css'
 import '@fortawesome/fontawesome-free/css/all.css'
@@ -14,6 +15,7 @@ Vue.config.productionTip = false
 Vue.use(Buefy, { defaultIconPack: 'fas' })
 Vue.use(VueAxios, axios.create({ baseURL: 'http://localhost:3000/api/' }))
 Vue.use(SimpleVueValidation, { mode: 'conservative' })
+promiseFinally.shim()
 
 new Vue({
   router,
