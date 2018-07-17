24e80597a86023afe1b8cd2acb3da6428a8e4434
Ben Walker <bwalke05@gmail.com>
2018-07-17 16:15:45 -0400

    Create validation mixin with all static login/signup validations

--- src/mixins/validation.js
+++ src/mixins/validation.js
@@ -1,3 +1,22 @@
-export default {
+import SimpleVueValidation from 'simple-vue-validator'
+const Validator = SimpleVueValidation.Validator
 
+export default {
+  validators: {
+    identifier: (value) => {
+      return Validator.value(value).required()
+    },
+    username: (value) => {
+      return Validator.value(value).required().minLength(4).maxLength(20)
+    },
+    email: (value) => {
+      return Validator.value(value).required().email()
+    },
+    password: (value) => {
+      return Validator.value(value).required().minLength(8)
+    },
+    'confirmPassword, password': (value, password) => {
+      return Validator.value(value).required().match(password)
+    }
+  }
 }
