eba47cf70240d40aaac76c17dc6d28af3ea43fc1
Ben Walker <bwalke05@gmail.com>
2018-07-17 12:36:40 -0400

    Remove validation from Login component

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,11 +1,9 @@
 <template lang="pug">
   #login
     .box
-      form(@submit.prevent="processForm" novalidate)
-        p.help.is-danger.is-pulled-right {{ validation.firstError('identifier') }}
+      form(@submit.prevent novalidate)
         b-field(label="Username or Email")
           b-input(v-model.trim="identifier" type="text" rounded ref="identifier")
-        p.help.is-danger.is-pulled-right {{ validation.firstError('password') }}
         b-field(label="Password")
           b-input(v-model="password" type="password" placeholder="••••••••" rounded password-reveal)
         .field
@@ -16,9 +14,6 @@
 </template>
 
 <script>
-import SimpleVueValidation from 'simple-vue-validator'
-const Validator = SimpleVueValidation.Validator
-
 export default {
   name: 'login',
   data () {
@@ -27,30 +22,6 @@ export default {
       password: '',
       rememberMe: false,
       loading: false
-    }
-  },
-  validators: {
-    identifier: value => { return Validator.value(value).required() },
-    password: value => { return Validator.value(value).required() }
-  },
-  methods: {
-    processForm () {
-      this.$validate().then(success => {
-        if (success) this.submit()
-      })
-    },
-    submit () {
-      this.loading = true
-      this.axios.post('login', {
-        identifier: this.identifier,
-        password: this.password
-      })
-        .then(() => this.$toast.open('Login succeeded.'))
-        .catch(() => this.$toast.open({
-          message: 'Login credentials not recognized.',
-          type: 'is-danger'
-        }))
-        .finally(() => { this.loading = false })
     }
   },
   mounted () {
