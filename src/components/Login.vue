a49fa199fa39c510c48d3bfd30d62a9b6cecf0ce
Ben Walker <bwalke05@gmail.com>
2018-07-17 16:59:22 -0400

    Add validation to login/signup components via mixin

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -3,20 +3,36 @@
     .box
       h1.subtitle.is-4.has-text-grey Welcome Back
       form(@submit.prevent novalidate)
-        b-field(label="Username or Email")
+
+        HelpText(dir="right" :msg="validation.firstError('identifier')")
+        b-field(label="Username or Email" :type="determineFieldType('identifier')")
           b-input(v-model.trim="identifier" type="text" rounded ref="identifier")
-        b-field(label="Password")
+
+        HelpText(dir="right" :msg="validation.firstError('password')")
+        b-field(label="Password" :type="determineFieldType('password')")
           b-input(v-model="password" type="password" placeholder="••••••••" rounded password-reveal)
+
         .field
           b-checkbox(v-model="rememberMe") Remember me
+
         .has-text-right
           button.button.is-primary.is-outlined(type="submit") Log In
+
       b-loading(:is-full-page="false" :active.sync="loading")
 </template>
 
 <script>
+import HelpText from '@/components/HelpText.vue'
+import validation from '@/mixins/validation'
+
 export default {
   name: 'login',
+  mixins: [
+    validation
+  ],
+  components: {
+    HelpText
+  },
   data () {
     return {
       identifier: '',
--- src/components/Signup.vue
+++ src/components/Signup.vue
@@ -3,22 +3,41 @@
     .box
       h1.subtitle.is-4.has-text-grey Create Your Account
       form(@submit.prevent novalidate)
-        b-field(label="Username")
+
+        HelpText(dir="right" :msg="validation.firstError('username')")
+        b-field(label="Username" :type="determineFieldType('username')")
           b-input(v-model.trim="username" type="text" rounded ref="username")
-        b-field(label="Email")
+
+        HelpText(dir="right" :msg="validation.firstError('email')")
+        b-field(label="Email" :type="determineFieldType('email')")
           b-input(v-model.trim="email" type="text" rounded)
-        b-field(label="Password")
+
+        HelpText(dir="right" :msg="validation.firstError('password')")
+        b-field(label="Password" :type="determineFieldType('password')")
           b-input(v-model="password" type="password" placeholder="••••••••" rounded password-reveal)
-        b-field(label="Confirm Password")
+
+        HelpText(dir="right" :msg="validation.firstError('confirmPassword')")
+        b-field(label="Confirm Password" :type="determineFieldType('confirmPassword')")
           b-input(v-model="confirmPassword" type="password" placeholder="••••••••" rounded password-reveal)
+
         .has-text-right
           button.button.is-primary.is-outlined(type="submit") Sign Up
+
       b-loading(:is-full-page="false" :active.sync="loading")
 </template>
 
 <script>
+import HelpText from '@/components/HelpText.vue'
+import validation from '@/mixins/validation'
+
 export default {
   name: 'signup',
+  mixins: [
+    validation
+  ],
+  components: {
+    HelpText
+  },
   data () {
     return {
       username: '',
