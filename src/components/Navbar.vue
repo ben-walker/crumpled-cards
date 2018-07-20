adb0ba0c402591f53dc60c1f9157e48f64e10e86
Ben Walker <bwalke05@gmail.com>
2018-07-20 15:11:08 -0400

    Add icons to router-links

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -6,8 +6,14 @@
           h1.title.is-2.has-text-primary {{ siteTitle }}
         .navbar-menu
           .navbar-end(v-if="!authenticated")
-            router-link.navbar-item(to="/login") Log In
-            router-link.navbar-item(to="/signup") Sign Up
+            router-link.navbar-item(to="/login")
+              span.icon.has-text-primary
+                i.fas.fa-unlock
+              span Log In
+            router-link.navbar-item(to="/signup")
+              span.icon.has-text-danger
+                i.fas.fa-door-open
+              span Sign Up
 </template>
 
 <script>
