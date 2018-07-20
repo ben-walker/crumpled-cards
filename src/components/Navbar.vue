8966b94eacc7a870897db6bfa7926d7f57c5843c
Ben Walker <bwalke05@gmail.com>
2018-07-20 14:37:23 -0400

    Rework navbar appearance

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,14 +1,13 @@
 <template lang="pug">
   #nav
-    nav.navbar.has-shadow.is-spaced.is-transparent.is-light
-      .navbar-brand
-        h1.title.is-2.has-text-primary {{ siteTitle }}
-      .navbar-menu
-        .navbar-end
-          .navbar-item(v-if="!authenticated")
-            router-link.button.is-primary(to="/login") Log In
-          .navbar-item(v-if="!authenticated")
-            router-link.button.is-primary(to="/signup") Sign Up
+    nav.navbar.has-shadow.is-spaced
+      .container
+        .navbar-brand
+          h1.title.is-2.has-text-primary {{ siteTitle }}
+        .navbar-menu
+          .navbar-end(v-if="!authenticated")
+            router-link.navbar-item(to="/login") Log In
+            router-link.navbar-item(to="/signup") Sign Up
 </template>
 
 <script>
