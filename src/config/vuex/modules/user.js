602e6acce363050662247714bf4cf92bcfb7b754
Ben Walker <bwalke05@gmail.com>
2018-07-20 14:25:58 -0400

    Separate user into vuex module

---
+++ src/config/vuex/modules/user.js
@@ -0,0 +1,33 @@
+import http from '@/config/axios'
+
+const state = {}
+
+const getters = {}
+
+const actions = {
+  login ({ commit }, authPayload) {
+    return new Promise((resolve, reject) => {
+      http.post('login', authPayload)
+        .then(res => resolve(res))
+        .catch(err => reject(err))
+    })
+  },
+
+  logout ({ commit }) {
+    return new Promise((resolve, reject) => {
+      http.post('logout')
+        .then(res => resolve(res))
+        .catch(err => reject(err))
+    })
+  }
+}
+
+const mutations = {}
+
+export default {
+  namespaced: true,
+  state,
+  getters,
+  actions,
+  mutations
+}
