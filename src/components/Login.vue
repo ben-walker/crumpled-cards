448e62c601deb693d06d8cb74903a872572f2b69
Ben Walker <bwalke05@gmail.com>
2018-07-20 16:23:21 -0400

    Access user state directly (no getter needed)

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -42,7 +42,7 @@ export default {
   },
   computed: {
     loading () {
-      return this.$store.getters['user/authPending']
+      return this.$store.state.user.authPending
     }
   },
   validators: {
