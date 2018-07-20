375518938681c0c17a31eb84082444a5232f850e
Ben Walker <bwalke05@gmail.com>
2018-07-20 09:40:19 -0400

    Remove route definitions from vue-router index

--- src/config/vue-router/index.js
+++ src/config/vue-router/index.js
@@ -1,17 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-
-import Login from '@/components/Login.vue'
-import Signup from '@/components/Signup.vue'
+import routes from './routes'
 
 Vue.use(Router)
-
-const routes = [
-  { path: '/', name: 'home' },
-  { path: '/login', name: 'login', component: Login },
-  { path: '/signup', name: 'signup', component: Signup },
-  { path: '*', redirect: { name: 'home' } }
-]
 
 export default new Router({
   mode: 'history',
