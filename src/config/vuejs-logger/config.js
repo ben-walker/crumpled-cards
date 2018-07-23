6ca14164596a4a388909d353fecc959a0aa8934b
Ben Walker <bwalke05@gmail.com>
2018-07-23 10:56:03 -0400

    Options for vuejs-logger

---
+++ src/config/vuejs-logger/config.js
@@ -0,0 +1,11 @@
+const debug = process.env.NODE_ENV !== 'production'
+
+export default {
+  isEnabled: debug,
+  logLevel: 'debug',
+  stringifyArguments: false,
+  showLogLevel: true,
+  showMethodName: true,
+  separator: '//',
+  showConsoleColors: true
+}
