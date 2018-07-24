b734312cd80b072f70593cc7fe5a24a88618a730
Ben Walker <bwalke05@gmail.com>
2018-07-24 17:46:34 -0400

    Signup view with all features

--- src/views/Signup.vue
+++ src/views/Signup.vue
@@ -1,9 +1,50 @@
 <template lang="pug">
   #signup
+    b-loading(:is-full-page="true" :active.sync="loading")
+    section.hero.is-fullheight.is-primary.is-bold
+      .hero-head
+        section.section.has-text-centered
+          h1.title.is-1 {{ $appName }}
+        .columns.is-centered
+          .column.is-one-third
+            .box
+              h2.subtitle.is-4.has-text-grey Create Your Account
+              SignupForm
+            nav.breadcrumb.has-bullet-separator.is-centered
+              ul
+                li
+                  router-link(to="/login")
+                    span.icon.has-text-white
+                      i.fas.fa-unlock
+                    span.has-text-white Log In
 </template>
 
 <script>
+import SignupForm from '@/components/SignupForm.vue'
+
 export default {
-  name: 'signup'
+  name: 'signup',
+  components: {
+    SignupForm
+  },
+  computed: {
+    loading () {
+      return this.$store.state.user.loading
+    }
+  },
+  methods: {
+    requestSignup (payload) {
+      this.$store.dispatch('user/signup', payload)
+        .catch(() => this.danger('Signup failed, please try again later.'))
+    },
+    danger (message) {
+      this.$toast.open({
+        message: message,
+        position: 'is-bottom',
+        type: 'is-warning',
+        queue: false
+      })
+    }
+  }
 }
 </script>
