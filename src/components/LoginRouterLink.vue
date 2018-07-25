ddeeaf065ad6469ff3c3503908d4fc0bd0bf7df3
Ben Walker <bwalke05@gmail.com>
2018-07-25 13:55:15 -0400

    router link components

---
+++ src/components/LoginRouterLink.vue
@@ -0,0 +1,19 @@
+<template lang="pug">
+  .loginRouterLink
+    router-link(to="/login" :class="textColor")
+      span.icon
+        i.fas.fa-unlock
+      span Log In
+</template>
+
+<script>
+export default {
+  name: 'loginRouterLink',
+  props: {
+    textColor: {
+      default: 'has-text-white',
+      type: String
+    }
+  }
+}
+</script>
