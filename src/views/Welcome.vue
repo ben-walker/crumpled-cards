ea5dbd5cd1ae3f1b4277d36fa95cc092e26fa6aa
Ben Walker <bwalke05@gmail.com>
2018-07-25 17:01:27 -0400

    Use TitleHead component, minor refactoring

--- src/views/Welcome.vue
+++ src/views/Welcome.vue
@@ -3,9 +3,8 @@
     section.hero.is-fullheight.is-primary.is-bold
       .hero-body
         .container.has-text-centered
-          h1.title.is-1 {{ $appName }}
-          h2.subtitle.is-4.
-            online poker, made with ❤️
+          TitleHead
+          h2.subtitle.is-4 online poker, made with ❤️
           nav.breadcrumb.has-bullet-separator.is-centered
             ul
               li #[LoginRouterLink]
@@ -13,12 +12,14 @@
 </template>
 
 <script>
+import TitleHead from '@/components/TitleHead.vue'
 import SignupRouterLink from '@/components/SignupRouterLink.vue'
 import LoginRouterLink from '@/components/LoginRouterLink.vue'
 
 export default {
   name: 'welcome',
   components: {
+    TitleHead,
     SignupRouterLink,
     LoginRouterLink
   }
