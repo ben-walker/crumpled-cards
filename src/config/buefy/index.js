da29056eea81847f3c8f5fdc71d1a4e2f6727938
Ben Walker <bwalke05@gmail.com>
2018-07-25 11:50:24 -0400

    Initialize buefy

---
+++ src/config/buefy/index.js
@@ -0,0 +1,7 @@
+import Vue from 'vue'
+import Buefy from 'buefy'
+
+import 'buefy/lib/buefy.min.css'
+import '@fortawesome/fontawesome-free/css/all.css'
+
+Vue.use(Buefy, { defaultIconPack: 'fas' })
