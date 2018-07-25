f947dcab3fc6d0ba5d8681ffe4d1d42206eed18a
Ben Walker <bwalke05@gmail.com>
2018-07-25 15:32:30 -0400

    Pass response directly to toast

--- src/views/Signup.vue
+++ src/views/Signup.vue
@@ -37,7 +37,7 @@ export default {
   methods: {
     requestSignup (payload) {
       this.$store.dispatch('user/signup', payload)
-        .catch(() => this.warnToast('Signup failed, please try again later.'))
+        .catch((err) => this.warnToast(err))
     }
   }
 }
