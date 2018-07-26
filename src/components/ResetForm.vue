db41f94e9153e1d28f94d812032f70986e8c0fcf
Ben Walker <bwalke05@gmail.com>
2018-07-26 12:53:35 -0400

    Handle user input

---
+++ src/components/ResetForm.vue
@@ -0,0 +1,24 @@
+<template lang="pug">
+  #resetForm
+    form(@submit.prevent="submit" novalidate)
+
+      b-field(label="Email")
+        b-input(v-model.trim="email" type="text" rounded ref="email")
+
+      .has-text-right
+        button.button.is-light(type="submit") Send Reset Link
+</template>
+
+<script>
+export default {
+  name: 'resetForm',
+  data () {
+    return {
+      email: ''
+    }
+  },
+  mounted () {
+    this.$refs.email.focus()
+  }
+}
+</script>
