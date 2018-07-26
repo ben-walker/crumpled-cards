6696edec5eac2c2ce743665c5ed355c1fdf8f915
Ben Walker <bwalke05@gmail.com>
2018-07-26 12:30:40 -0400

    Add general outline to reset view

--- src/views/Reset.vue
+++ src/views/Reset.vue
@@ -1,9 +1,22 @@
 <template lang="pug">
   #reset
+    section.hero.is-fullheight
+      .hero-head
+        section.section.has-text-centered
+          TitleHead
+        .columns.is-centered
+          .column.is-one-third
+            .box
+              h2.subtitle.is-4.has-text-grey Reset Your Password
 </template>
 
 <script>
+import TitleHead from '@/components/TitleHead.vue'
+
 export default {
-  name: 'reset'
+  name: 'reset',
+  components: {
+    TitleHead
+  }
 }
 </script>
