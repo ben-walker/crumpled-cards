97f26a5f8e00dd0abdfa8f0ed6d8b267e7371f09
Ben Walker <bwalke05@gmail.com>
2018-07-26 12:58:51 -0400

    Validate on submit

--- src/components/ResetForm.vue
+++ src/components/ResetForm.vue
@@ -2,7 +2,9 @@
   #resetForm
     form(@submit.prevent="submit" novalidate)
 
-      b-field(label="Email")
+      b-field(label="Email"
+      :type="fieldType('email')"
+      :message="validation.firstError('email')")
         b-input(v-model.trim="email" type="text" rounded ref="email")
 
       .has-text-right
@@ -10,11 +12,28 @@
 </template>
 
 <script>
+import SimpleVueValidator from 'simple-vue-validator'
+
+const Validator = SimpleVueValidator.Validator
+
 export default {
   name: 'resetForm',
   data () {
     return {
       email: ''
+    }
+  },
+  validators: {
+    email: (value) => {
+      return Validator.value(value).required().email()
+    }
+  },
+  methods: {
+    submit () {
+      this.$validate()
+    },
+    fieldType (field) {
+      return this.validation.hasError(field) ? 'is-danger' : ''
     }
   },
   mounted () {
