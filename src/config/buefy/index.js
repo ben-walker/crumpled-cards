bf2801127c8c07c03c15e047055fe7a992d9d647
Ben Walker <bwalke05@gmail.com>
2018-07-26 08:41:13 -0400

    Remove buefy css import

--- src/config/buefy/index.js
+++ src/config/buefy/index.js
@@ -1,7 +1,6 @@
 import Vue from 'vue'
 import Buefy from 'buefy'
 
-import 'buefy/lib/buefy.min.css'
 import '@fortawesome/fontawesome-free/css/all.css'
 
 Vue.use(Buefy, { defaultIconPack: 'fas' })
