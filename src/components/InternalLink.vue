daef81763e3fd42b20432adf9910dfe57715e325
Ben Walker <bwalke05@gmail.com>
2018-07-26 16:18:29 -0400

    Generalized component for router-links

---
+++ src/components/InternalLink.vue
@@ -0,0 +1,14 @@
+<template lang="pug">
+  .internalLink
+    router-link(:to="link.route" :class="'has-text-' + link.color")
+      span.icon
+        i.fas(:class="'fa-' + link.icon")
+      span {{ link.text }}
+</template>
+
+<script>
+export default {
+  name: 'internalLink',
+  props: ['link']
+}
+</script>
