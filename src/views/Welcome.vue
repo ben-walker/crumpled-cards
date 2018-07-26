0c6e7f00e7604cb999606d8593d456bad85dc871
Ben Walker <bwalke05@gmail.com>
2018-07-26 16:27:11 -0400

    Use general router-link component

--- src/views/Welcome.vue
+++ src/views/Welcome.vue
@@ -7,21 +7,27 @@
           h2.subtitle.is-4.has-text-white online poker, made with ❤️
           nav.breadcrumb.has-bullet-separator.is-centered
             ul
-              li #[LoginRouterLink]
-              li #[SignupRouterLink]
+              li(v-for="link in links" :key="link.id")
+                InternalLink(:link="link")
 </template>
 
 <script>
 import TitleHead from '@/components/TitleHead.vue'
-import SignupRouterLink from '@/components/SignupRouterLink.vue'
-import LoginRouterLink from '@/components/LoginRouterLink.vue'
+import InternalLink from '@/components/InternalLink.vue'
 
 export default {
   name: 'welcome',
+  data () {
+    return {
+      links: [
+        { id: 1, route: '/login', color: 'white', icon: 'unlock', text: 'Log In' },
+        { id: 2, route: '/signup', color: 'white', icon: 'clone', text: 'Sign Up' }
+      ]
+    }
+  },
   components: {
     TitleHead,
-    SignupRouterLink,
-    LoginRouterLink
+    InternalLink
   }
 }
 </script>
