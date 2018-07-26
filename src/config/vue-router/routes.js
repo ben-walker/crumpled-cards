d0a8ab0ff53e4c8fa6f498abc6d6300a08fb614b
Ben Walker <bwalke05@gmail.com>
2018-07-26 12:10:27 -0400

    Add reset view to router

--- src/config/vue-router/routes.js
+++ src/config/vue-router/routes.js
@@ -1,10 +1,12 @@
 import Welcome from '@/views/Welcome.vue'
 import Login from '@/views/Login.vue'
 import Signup from '@/views/Signup.vue'
+import Reset from '@/views/Reset.vue'
 
 export default [
   { path: '/', component: Welcome },
   { path: '/login', component: Login },
   { path: '/signup', component: Signup },
+  { path: '/reset', component: Reset },
   { path: '*', redirect: '/' }
 ]
