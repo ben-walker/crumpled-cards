ed2c342bf36bd9b13c2305f0d84126ff2e719e73
Ben Walker <bwalke05@gmail.com>
2018-07-26 09:01:45 -0400

    Use has-text-white as default text color

--- src/components/TitleHead.vue
+++ src/components/TitleHead.vue
@@ -1,10 +1,16 @@
 <template lang="pug">
   #titleHead
-    h1.title.is-1 {{ $appName }}
+    h1.title.is-1(:class="textColor") {{ $appName }}
 </template>
 
 <script>
 export default {
-  name: 'titleHead'
+  name: 'titleHead',
+  props: {
+    textColor: {
+      default: 'has-text-white',
+      type: String
+    }
+  }
 }
 </script>
