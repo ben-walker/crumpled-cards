d581181d901fec02b6e683b936b03af11becfe5a
Ben Walker <bwalke05@gmail.com>
2018-07-27 17:26:21 -0400

    Include socket module in Vuex store

--- src/config/vuex/index.js
+++ src/config/vuex/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 import user from './modules/user'
+import socket from './modules/socket'
 
 Vue.use(Vuex)
 
@@ -8,7 +9,8 @@ const debug = process.env.NODE_ENV !== 'production'
 
 export default new Vuex.Store({
   modules: {
-    user
+    user,
+    socket
   },
   strict: debug
 })
