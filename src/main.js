f58cfa5549532fd6ae3bfdec5c36d61b1706c904
Ben Walker <bwalke05@gmail.com>
2018-07-30 12:49:10 -0400

    Change order of imports

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
 import SimpleVueValidation from 'simple-vue-validator'
+import promiseFinally from 'promise.prototype.finally'
 import router from '@/config/vue-router'
 import store from '@/config/vuex'
 import http from '@/config/axios'
@@ -8,7 +9,6 @@ import '@/config/vuejs-logger'
 import '@/config/buefy'
 import '@/config/vue-lodash'
 import '@/config/vue-socket.io'
-import promiseFinally from 'promise.prototype.finally'
 
 Vue.config.productionTip = false
 Vue.use(SimpleVueValidation)
