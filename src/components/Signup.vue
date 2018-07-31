4b18ca0c4dd54aafaed457829c950f5c9ca5f9a2
Ben Walker <bwalke05@gmail.com>
2018-07-31 16:03:50 -0400

    Add validations/emits

--- src/components/Signup.vue
+++ src/components/Signup.vue
@@ -3,11 +3,17 @@
     .box
       h2.subtitle.is-4.has-text-grey Create Your Account
       form(@submit.prevent="submit" novalidate)
-        b-field(label="Username")
+        b-field(label="Username"
+        :type="fieldType('username')"
+        :message="validation.firstError('username')")
           b-input(v-model.trim="username" type="text" rounded ref="username")
-        b-field(label="Email")
+        b-field(label="Email"
+        :type="fieldType('email')"
+        :message="validation.firstError('email')")
           b-input(v-model.trim="email" type="text" rounded)
-        b-field(label="Password")
+        b-field(label="Password"
+        :type="fieldType('password')"
+        :message="validation.firstError('password')")
           b-input(v-model="password" type="password" placeholder="••••••••" rounded password-reveal)
         .has-text-right
           button.button.is-light(type="submit") Sign Up
@@ -31,8 +37,81 @@ export default {
       loginLink: { route: '/authenticate/login', color: 'primary', text: 'Log In' }
     }
   },
+  computed: {
+    authPayload () {
+      return {
+        username: this.username,
+        email: this.email,
+        password: this.password
+      }
+    }
+  },
   components: {
     InternalLink
+  },
+  mixins: [
+    FieldType
+  ],
+  validators: {
+    username: {
+      cache: true,
+      debounce: 200,
+      validator: function (value) {
+        let test = Validator.value(value).required().minLength(4).maxLength(20)
+        if (!test.hasImmediateError()) {
+          test.custom(() => {
+            return new Promise((resolve, reject) => {
+              this.$http.get('usernameRegistered', {
+                params: { username: this.username }
+              })
+                .then(res => {
+                  res.data.userFound
+                    ? resolve('Username taken.')
+                    : resolve()
+                })
+                .catch(err => reject(err))
+            })
+          })
+        }
+        return test
+      }
+    },
+    email: {
+      cache: true,
+      debounce: 200,
+      validator: function (value) {
+        let test = Validator.value(value).required().email()
+        if (!test.hasImmediateError()) {
+          test.custom(() => {
+            return new Promise((resolve, reject) => {
+              this.$http.get('emailRegistered', {
+                params: { email: this.email }
+              })
+                .then(res => {
+                  res.data.userFound
+                    ? resolve('Email taken.')
+                    : resolve()
+                })
+                .catch(err => reject(err))
+            })
+          })
+        }
+        return test
+      }
+    },
+    password: (value) => {
+      return Validator.value(value).required().minLength(8)
+    }
+  },
+  methods: {
+    submit () {
+      this.$validate().then(success => {
+        if (success) this.$emit('signup', this.authPayload)
+      })
+    }
+  },
+  mounted () {
+    this.$refs.username.focus()
   }
 }
 </script>
