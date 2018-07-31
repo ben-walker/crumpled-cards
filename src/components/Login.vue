ccd7130352e30c5545934fd10dc5dc3df44c0d78
Ben Walker <bwalke05@gmail.com>
2018-07-31 15:48:58 -0400

    Add validations and emits

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -3,9 +3,13 @@
     .box
       h2.subtitle.is-4.has-text-grey Welcome Back
       form(@submit.prevent="submit" novalidate)
-        b-field(label="Username or Email")
-          b-input(v-model.trim="identifier" type="text" rounded red="identifier")
-        b-field(label="Password")
+        b-field(label="Username or Email"
+        :type="fieldType('identifier')"
+        :message="validation.firstError('identifier')")
+          b-input(v-model.trim="identifier" type="text" rounded ref="identifier")
+        b-field(label="Password"
+        :type="fieldType('password')"
+        :message="validation.firstError('password')")
           b-input(v-model="password" type="password" placeholder="••••••••" rounded password-reveal)
         nav.level
           .level-left
@@ -18,7 +22,11 @@
 </template>
 
 <script>
+import SimpleVueValidator from 'simple-vue-validator'
+import FieldType from '@/mixins/FieldType'
 import InternalLink from '@/components/InternalLink.vue'
+
+const Validator = SimpleVueValidator.Validator
 
 export default {
   name: 'login',
@@ -29,8 +37,62 @@ export default {
       signupLink: { route: '/authenticate/signup', color: 'primary', text: 'Sign Up' }
     }
   },
+  computed: {
+    authPayload () {
+      return {
+        identifier: this.identifier,
+        password: this.password
+      }
+    }
+  },
   components: {
     InternalLink
+  },
+  mixins: [
+    FieldType
+  ],
+  validators: {
+    identifier: {
+      cache: true,
+      debounce: 200,
+      validator: function (value) {
+        let test = Validator.value(value).required()
+        if (!test.hasImmediateError()) {
+          test.custom(() => {
+            return new Promise((resolve, reject) => {
+              this.$http.get('identifierExists', {
+                params: { identifier: this.identifier }
+              })
+                .then(res => {
+                  res.data.userFound
+                    ? resolve()
+                    : resolve('User not found.')
+                })
+                .catch(err => reject(err))
+            })
+          })
+        }
+        return test
+      }
+    },
+    password: (value) => {
+      return Validator.value(value).required()
+    }
+  },
+  methods: {
+    submit () {
+      this.$validate().then(success => {
+        if (success) this.$emit('login', this.authPayload)
+      })
+    },
+    forgotPassword () {
+      this.$validate('identifier').then(success => {
+        if (success) this.$emit('forgotPassword', this.identifier)
+      })
+    }
+  },
+  mounted () {
+    this.$refs.identifier.focus()
   }
 }
 </script>
