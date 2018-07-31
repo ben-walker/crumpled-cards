a2cb89d35afab7e3d2b1e3968e0b96d4bb259c57
Ben Walker <bwalke05@gmail.com>
2018-07-31 15:53:00 -0400

    Catch login event from Login component

--- src/views/Authenticate.vue
+++ src/views/Authenticate.vue
@@ -7,16 +7,27 @@
         .columns.is-centered
           .column.is-one-third
             transition(name="fade" mode="out-in")
-              keep-alive #[router-view]
+              keep-alive
+                router-view(@login="requestLogin")
 </template>
 
 <script>
 import TitleHead from '@/components/TitleHead.vue'
+import Toast from '@/mixins/Toast'
 
 export default {
   name: 'authenticate',
   components: {
     TitleHead
+  },
+  mixins: [
+    Toast
+  ],
+  methods: {
+    requestLogin (payload) {
+      this.$store.dispatch('user/login', payload)
+        .catch(err => this.dangerToast(err))
+    }
   }
 }
 </script>
