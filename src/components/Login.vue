395ab5e6ddce019b5872b476d10b445a34b83418
Ben Walker <bwalke05@gmail.com>
2018-07-31 14:40:27 -0400

    component to handle logging in

---
+++ src/components/Login.vue
@@ -0,0 +1,36 @@
+<template lang="pug">
+  #login
+    .box
+      h2.subtitle.is-4.has-text-grey Welcome Back
+      form(@submit.prevent="submit" novalidate)
+        b-field(label="Username or Email")
+          b-input(v-model.trim="identifier" type="text" rounded red="identifier")
+        b-field(label="Password")
+          b-input(v-model="password" type="password" placeholder="••••••••" rounded password-reveal)
+        nav.level
+          .level-left
+            .level-item
+              a(@click="forgotPassword") Forgot your password?
+          .level-right
+            .level-item
+              button.button.is-light(type="submit") Log In
+        p.is-size-7 Need an account? #[InternalLink(:link="signupLink")]
+</template>
+
+<script>
+import InternalLink from '@/components/InternalLink.vue'
+
+export default {
+  name: 'login',
+  data () {
+    return {
+      identifier: '',
+      password: '',
+      signupLink: { route: '/authenticate/signup', color: 'primary', text: 'Sign Up' }
+    }
+  },
+  components: {
+    InternalLink
+  }
+}
+</script>
