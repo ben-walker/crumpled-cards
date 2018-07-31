d2d1da2eb05877088e86e453d0ee61cb4192e27e
Ben Walker <bwalke05@gmail.com>
2018-07-31 14:52:17 -0400

    Initial layout of signup component

---
+++ src/components/Signup.vue
@@ -0,0 +1,34 @@
+<template lang="pug">
+  #signup
+    .box
+      h2.subtitle.is-4.has-text-grey Create Your Account
+      form(@submit.prevent="submit" novalidate)
+        b-field(label="Username")
+          b-input(v-model.trim="username" type="text" rounded ref="username")
+        b-field(label="Email")
+          b-input(v-model.trim="email" type="text" rounded)
+        b-field(label="Password")
+          b-input(v-model="password" type="password" placeholder="••••••••" rounded password-reveal)
+        .has-text-right
+          button.button.is-light(type="submit") Sign Up
+        p.help.has-text-centered Already have an account? #[InternalLink(:link="loginLink")]
+</template>
+
+<script>
+import InternalLink from '@/components/InternalLink.vue'
+
+export default {
+  name: 'signup',
+  data () {
+    return {
+      username: '',
+      email: '',
+      password: '',
+      loginLink: { route: '/authenticate/login', color: 'primary', text: 'Log In' }
+    }
+  },
+  components: {
+    InternalLink
+  }
+}
+</script>
