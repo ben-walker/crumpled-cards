12b87d82a7c1f914d05086d6727cc78adcf09313
Ben Walker <bwalke05@gmail.com>
2018-07-31 14:04:10 -0400

    Redefine routes to nest login/signup within authenticate

--- src/config/vue-router/routes.js
+++ src/config/vue-router/routes.js
@@ -1,10 +1,17 @@
 import Welcome from '@/views/Welcome.vue'
+import Authenticate from '@/views/Authenticate.vue'
 import Login from '@/views/Login.vue'
 import Signup from '@/views/Signup.vue'
 
 export default [
   { path: '/', component: Welcome },
-  { path: '/login', component: Login },
-  { path: '/signup', component: Signup },
+  { path: '/authenticate',
+    component: Authenticate,
+    children: [
+      { path: 'login', component: Login },
+      { path: 'signup', component: Signup },
+      { path: '', component: Login }
+    ]
+  },
   { path: '*', redirect: '/' }
 ]
