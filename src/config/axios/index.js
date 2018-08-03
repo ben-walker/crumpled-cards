4b5ad6185f557e917281378bd92cc7644962bcea
Ben Walker <bwalke05@gmail.com>
2018-08-03 16:49:30 -0400

    Simplify axios setup by using vue-axios

--- src/config/axios/index.js
+++ src/config/axios/index.js
@@ -1,10 +1,6 @@
+import Vue from 'vue'
 import axios from 'axios'
+import VueAxios from 'vue-axios'
 import config from './config'
-import errorHandler from './error-handler'
 
-const http = axios.create(config)
-
-export {
-  http as default,
-  errorHandler
-}
+Vue.use(VueAxios, axios.create(config))
