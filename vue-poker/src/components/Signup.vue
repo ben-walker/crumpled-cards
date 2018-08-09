d45ed25c864db594169dbd7bab067dad661ac58a
Ben Walker <bwalke05@gmail.com>
2018-08-09 10:23:08 -0400

    Extract unique validator into function

--- vue-poker/src/components/Signup.vue
+++ vue-poker/src/components/Signup.vue
@@ -20,6 +20,17 @@ import { required, email, minLength, maxLength } from 'vuelidate/lib/validators'
 import InternalLink from '@/components/InternalLink.vue'
 import minDelay from 'p-min-delay'
 
+const unique = (field) => (value, vm) => {
+  if (value === '') return true
+  return minDelay(new Promise((resolve, reject) => {
+    vm.$http.get(`${field}Registered`, {
+      params: { [field]: value }
+    })
+      .then(res => resolve(!res.data))
+      .catch(err => reject(err))
+  }), 1000)
+}
+
 export default {
   name: 'signup',
   data () {
@@ -50,30 +61,12 @@ export default {
       required,
       minLength: minLength(4),
       maxLength: maxLength(20),
-      unique (value) {
-        if (value === '') return true
-        return minDelay(new Promise((resolve, reject) => {
-          this.$http.get('usernameRegistered', {
-            params: { username: value }
-          })
-            .then(res => resolve(!res.data))
-            .catch(err => reject(err))
-        }), 1000)
-      }
+      unique: unique('username')
     },
     email: {
       required,
       email,
-      unique (value) {
-        if (value === '') return true
-        return minDelay(new Promise((resolve, reject) => {
-          this.$http.get('emailRegistered', {
-            params: { email: value }
-          })
-            .then(res => resolve(!res.data))
-            .catch(err => reject(err))
-        }), 1000)
-      }
+      unique: unique('email')
     },
     password: {
       required,
