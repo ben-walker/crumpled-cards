cb096a5e8232b7986503fc2312baa1b0e9351be2
Ben Walker <bwalke05@gmail.com>
2018-08-13 16:41:09 -0400

    Route to '/' if authenticated and accessing a route with no auth requirement

--- vue-poker/src/config/vue-router/guards/index.js
+++ vue-poker/src/config/vue-router/guards/index.js
@@ -5,6 +5,7 @@ export default (router) => {
     const authRequired = to.matched.some((route) => route.meta.auth)
     const authenticated = store.state.user.authenticated
     if (authRequired && !authenticated) next('/welcome')
+    else if (!authRequired && authenticated) next('/')
     else next()
   })
 }
