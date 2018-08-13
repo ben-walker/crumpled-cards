516315bd216adedb88d05d974c34905be3835fc8
Ben Walker <bwalke05@gmail.com>
2018-08-13 11:20:51 -0400

    tool tip with question mark icon

---
+++ vue-poker/src/components/ToolTip.vue
@@ -0,0 +1,11 @@
+<template lang="pug">
+  b-tooltip(:label="text" animated multilined)
+    i.has-text-primary.fas.fa-question-circle.fa-lg
+</template>
+
+<script>
+export default {
+  name: 'toolTip',
+  props: ['text']
+}
+</script>
