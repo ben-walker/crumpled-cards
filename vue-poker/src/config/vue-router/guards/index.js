47973dd3176d965cabb60ab98e2284e1709ba7fb
Ben Walker <bwalke05@gmail.com>
2018-08-13 16:08:22 -0400

    Navigation guards

---
+++ vue-poker/src/config/vue-router/guards/index.js
@@ -0,0 +1,10 @@
+import store from '@/config/vuex'
+
+export default (router) => {
+  router.beforeEach((to, from, next) => {
+    const authRequired = to.matched.some((route) => route.meta.auth)
+    const authenticated = store.state.user.authenticated
+    if (authRequired && !authenticated) next('/welcome')
+    else next()
+  })
+}
