f49221371683d599be9a2f326eced5808e9c1c74
Ben Walker <bwalke05@gmail.com>
2018-08-13 12:15:18 -0400

    Measure and display password strength with zxcvbn

---
+++ vue-poker/src/components/PasswordStrengthMeter.vue
@@ -0,0 +1,42 @@
+<template lang="pug">
+  .passwordStrengthMeter
+    label.label Password Strength #[ToolTip(:text="helpText")]
+    progress.progress.is-marginless(:value="strengthScore" :class="strengthColor" max="4")
+    p.help.label {{ warning }}
+</template>
+
+<script>
+import zxcvbn from 'zxcvbn'
+import ToolTip from './ToolTip.vue'
+
+export default {
+  name: 'passwordStrengthMeter',
+  data () {
+    return {
+      helpText: 'Measured in bits of entropy, compared against common passwords.'
+    }
+  },
+  props: ['password'],
+  computed: {
+    zxcvbnResult () {
+      return zxcvbn(this.password)
+    },
+    strengthScore () {
+      return this.zxcvbnResult.score
+    },
+    strengthColor () {
+      switch (this.strengthScore) {
+        case 0: case 1: return 'is-danger'
+        case 2: case 3: return 'is-warning'
+        case 4: return 'is-success'
+      }
+    },
+    warning () {
+      return this.zxcvbnResult.feedback.warning
+    }
+  },
+  components: {
+    ToolTip
+  }
+}
+</script>
