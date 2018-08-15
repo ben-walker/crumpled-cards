1f78843c936e5af71377df3e11251d717f98ff03
Ben Walker <bwalke05@gmail.com>
2018-08-15 17:20:55 -0400

    Comment out code which can (hopefully) be used to reduce bundle size

--- vue-poker/src/config/buefy/index.js
+++ vue-poker/src/config/buefy/index.js
@@ -2,3 +2,16 @@ import Vue from 'vue'
 import Buefy from 'buefy'
 
 Vue.use(Buefy, { defaultIconPack: 'fas' })
+/* import config from 'buefy/src/utils/config'
+import Field from 'buefy/dist/components/field'
+import Input from 'buefy/dist/components/input'
+import Icon from 'buefy/dist/components/icon'
+import Toast from 'buefy/dist/components/toast'
+import Modal from 'buefy/dist/components/modal'
+
+Vue.use(Field)
+Vue.use(Input)
+Vue.use(Icon)
+Vue.use(Toast)
+Vue.use(Modal)
+config.defaultIconPack = 'fas' */
