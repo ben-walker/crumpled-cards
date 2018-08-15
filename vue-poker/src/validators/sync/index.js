b110d99b9c4f6b19d8a18a4143262caebf312fe7
Ben Walker <bwalke05@gmail.com>
2018-08-15 11:22:28 -0400

    Don't import zxcvbn directly

--- vue-poker/src/validators/sync/index.js
+++ vue-poker/src/validators/sync/index.js
@@ -1,6 +1,4 @@
-import zxcvbn from 'zxcvbn'
-
-const securePassword = (value) => zxcvbn(value).score > 2
+const securePassword = (zxcvbnScore) => (value) => zxcvbnScore > 2
 
 const manualError = (trigger) => (value) => !trigger
 
