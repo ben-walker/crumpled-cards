27ac02d37c324ccc05bed551f8d0fbcd6af3edd9
Ben Walker <bwalke05@gmail.com>
2018-08-16 12:51:51 -0400

    Enable credentials only on login POST

--- vue-poker/src/config/vuex/modules/user.js
+++ vue-poker/src/config/vuex/modules/user.js
@@ -25,7 +25,7 @@ const actions = {
   login ({ commit }, authPayload) {
     commit('startLoading')
     return new Promise((resolve, reject) => {
-      Vue.axios.post('login', authPayload)
+      Vue.axios.post('login', authPayload, { withCredentials: true })
         .then(res => {
           commit('authenticate')
           resolve(res)
