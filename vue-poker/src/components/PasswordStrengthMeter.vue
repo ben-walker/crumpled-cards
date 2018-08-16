50c6e3ded1d9628575228528e778088d4edd5a62
Ben Walker <bwalke05@gmail.com>
2018-08-16 17:03:55 -0400

    Use entropy instead of zxcvbn

--- vue-poker/src/components/PasswordStrengthMeter.vue
+++ vue-poker/src/components/PasswordStrengthMeter.vue
@@ -1,28 +1,25 @@
 <template lang="pug">
   .passwordStrengthMeter
     label.label Password Strength
-    progress.progress.is-marginless.is-small(:value="strengthScore" :class="strengthColor" max="4")
-    p.help.label {{ warning }}
+    progress.progress.is-small(:value="strength" :class="strengthColor" :max="MAX")
 </template>
 
 <script>
 export default {
   name: 'passwordStrengthMeter',
-  props: ['zxcvbnResult'],
+  data () {
+    return {
+      MAX: 128
+    }
+  },
+  props: ['strength'],
   computed: {
-    strengthScore () {
-      return this.zxcvbnResult.score
-    },
     strengthColor () {
-      switch (this.strengthScore) {
-        case 0: case 1: return 'is-danger'
-        case 2: return 'is-warning'
-        case 3: return 'is-success'
-        case 4: return 'is-primary'
-      }
-    },
-    warning () {
-      return this.zxcvbnResult.feedback.warning
+      const quotient = this.strength / this.MAX
+      if (quotient < 0.2) return 'is-danger'
+      else if (quotient > 0.2 && quotient < 0.6) return 'is-warning'
+      else if (quotient > 0.6 && quotient < 0.8) return 'is-success'
+      else if (quotient > 0.8) return 'is-primary'
     }
   }
 }
