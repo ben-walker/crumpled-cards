6cc5d82406abce52027f09b65d77f48f673345db
Ben Walker <bwalke05@gmail.com>
2018-08-16 16:43:52 -0400

    Import entropy measurement

--- vue-poker/src/components/Signup.vue
+++ vue-poker/src/components/Signup.vue
@@ -37,11 +37,12 @@
 
 <script>
 import { validationMixin } from 'vuelidate'
-import { debounceInput, toast, zxcvbn } from '@/mixins'
 import { required, email, minLength, maxLength } from 'vuelidate/lib/validators'
 import { securePassword, unique } from '@/validators'
 import InternalLink from '@/components/InternalLink.vue'
 import PasswordStrengthMeter from '@/components/PasswordStrengthMeter.vue'
+import { debounceInput, toast } from '@/mixins'
+import measureEntropy from 'fast-password-entropy'
 
 export default {
   name: 'signup',
