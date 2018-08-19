e8db614f4ce40d21a95316077d1be7dc6b65783d
Ben Walker <bwalke05@gmail.com>
2018-08-18 20:09:53 -0400

    Convert vue-wait setup to ts

--- vue-poker/src/config/wait/index.js
+++
@@ -1,7 +0,0 @@
-import Vue from 'vue'
-import VueWait from 'vue-wait'
-import config from './config'
-
-Vue.use(VueWait)
-
-export default new VueWait(config)
--- vue-poker/src/config/wait/index.ts
+++ vue-poker/src/config/wait/index.ts
@@ -1,8 +1,13 @@
-export default {
+import Vue from 'vue'
+import VueWait from 'vue-wait'
+
+Vue.use(VueWait)
+
+export default new VueWait({
   useVuex: true,
   vuexModuleName: 'wait',
   registerComponent: true,
   componentName: 'v-wait',
   registerDirective: true,
   directiveName: 'wait'
-}
+})
