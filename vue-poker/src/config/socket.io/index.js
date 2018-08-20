8e8b2b37d5ca78a0dec12fc24064b71940d8d211
Ben Walker <bwalke05@gmail.com>
2018-08-20 12:22:31 -0400

    Use custom socket implementation

--- vue-poker/src/config/socket.io/index.js
+++ vue-poker/src/config/socket.io/index.js
@@ -1,5 +1,8 @@
 import Vue from 'vue'
+import io from 'socket.io-client'
 import VueSocketIO from 'vue-socket.io'
 import store from '@/config/vuex'
 
-Vue.use(VueSocketIO, process.env.VUE_APP_SERVER, store)
+export const socketInstance = io(process.env.VUE_APP_SERVER)
+
+Vue.use(VueSocketIO, socketInstance, store)
