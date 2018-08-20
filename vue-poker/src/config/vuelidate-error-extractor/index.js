4ede4ce9810e89ac9c7b9ac5302185fb1d61ef28
Ben Walker <bwalke05@gmail.com>
2018-08-20 14:18:31 -0400

    Simplify setup

--- vue-poker/src/config/vuelidate-error-extractor/index.js
+++ vue-poker/src/config/vuelidate-error-extractor/index.js
@@ -1,9 +1,9 @@
 import Vue from 'vue'
 import VuelidateErrorExtractor from 'vuelidate-error-extractor'
-import Template from './Template.vue'
+import template from './Template.vue'
 import messages from './messages'
 
 Vue.use(VuelidateErrorExtractor, {
-  template: Template,
-  messages: messages
+  template,
+  messages
 })
