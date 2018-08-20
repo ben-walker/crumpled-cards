56bfa9f74641b696529c88d093f945ce11e6c26e
Ben Walker <bwalke05@gmail.com>
2018-08-20 15:16:00 -0400

    Fix typo ('mathed' -> 'matched')

--- vue-poker/src/config/router/guards/authCheck.js
+++ vue-poker/src/config/router/guards/authCheck.js
@@ -1,7 +1,7 @@
 import store from '@/config/vuex'
 
 export default (to, from, next) => {
-  const authRequired = to.mathed.some((route) => route.meta.auth)
+  const authRequired = to.matched.some((route) => route.meta.auth)
   const authenticated = store.state.user.authenticated
 
   if (authRequired && !authenticated) next('/welcome')
