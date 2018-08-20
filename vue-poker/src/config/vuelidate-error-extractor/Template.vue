81d6a9809b7d59dbef392aa63ac569634da85648
Ben Walker <bwalke05@gmail.com>
2018-08-20 11:20:57 -0400

    Use ts for component

--- vue-poker/src/config/vuelidate-error-extractor/Template.vue
+++ vue-poker/src/config/vuelidate-error-extractor/Template.vue
@@ -8,27 +8,28 @@
     slot
 </template>
 
-<script>
+<script lang="ts">
+import Vue from 'vue'
 import { singleErrorExtractorMixin } from 'vuelidate-error-extractor'
 
-export default {
+export default Vue.extend({
   extends: singleErrorExtractorMixin,
   computed: {
-    type () {
+    type (): string {
       return this.hasErrors
         ? 'is-danger'
         : null
     },
-    customClass () {
+    customClass (): string {
       return this.hasErrors
         ? 'has-text-danger'
         : null
     },
-    errorMessage () {
+    errorMessage (): string {
       return this.hasErrors
         ? this.firstErrorMessage
         : null
     }
   }
-}
+})
 </script>
