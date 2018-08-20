eae67640883b325df8f9bdf04bf23c6604868686
Ben Walker <bwalke05@gmail.com>
2018-08-20 10:55:51 -0400

    Buefy setup ts conversion

---
+++ vue-poker/src/config/buefy/index.ts
@@ -0,0 +1,15 @@
+import Vue from 'vue'
+import Buefy from 'buefy'
+
+// TODO: import and use only needed components, see below for an example
+/*
+import config from 'buefy/src/utils/config'
+import Field from 'buefy/dist/components/field'
+
+Vue.use(Field)
+config.defaultIconPack = 'fas'
+*/
+
+Vue.use(Buefy, {
+  defaultIconPack: 'fas'
+})
