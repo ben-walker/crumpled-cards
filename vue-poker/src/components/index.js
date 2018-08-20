e7874ddc4b007b29647bbfb404653f457b8dae86
Ben Walker <bwalke05@gmail.com>
2018-08-20 14:13:01 -0400

    Create index for easier component importing

---
+++ vue-poker/src/components/index.js
@@ -0,0 +1,15 @@
+import Login from './Login.vue'
+import Signup from './Signup.vue'
+import TitleHead from './TitleHead.vue'
+import ToolTip from './ToolTip.vue'
+import InternalLink from './InternalLink.vue'
+import PasswordStrengthMeter from './PasswordStrengthMeter.vue'
+
+export {
+  Login,
+  Signup,
+  TitleHead,
+  ToolTip,
+  InternalLink,
+  PasswordStrengthMeter
+}
