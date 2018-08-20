1bdcdbc74b90078466c481f8e2c5261a85255844
Ben Walker <bwalke05@gmail.com>
2018-08-20 12:20:14 -0400

    Refactor axios setup without VueAxios

--- vue-poker/src/config/axios/index.js
+++ vue-poker/src/config/axios/index.js
@@ -1,6 +1,8 @@
-import Vue from 'vue'
 import axios from 'axios'
-import VueAxios from 'vue-axios'
-import config from './config'
 
-Vue.use(VueAxios, axios.create(config))
+const http = axios.create({
+  baseURL: process.env.VUE_APP_API,
+  withCredentials: true
+})
+
+export default http
