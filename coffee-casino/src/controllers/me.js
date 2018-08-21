3bff5e9d9f24c0e8eff41c40269ee5c6941be872
Ben Walker <bwalke05@gmail.com>
2018-08-21 16:59:45 -0400

    Fix typo

--- coffee-casino/src/controllers/me.js
+++ coffee-casino/src/controllers/me.js
@@ -1,4 +1,4 @@
-export const getUserInfp = (req, res, next) => {
+export const getUserInfo = (req, res, next) => {
   req.isAuthenticated()
     ? res.status(200).send({ user: req.user })
     : res.status(404).send({ user: null })
