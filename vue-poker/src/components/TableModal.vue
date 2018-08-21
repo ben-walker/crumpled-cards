49a66e63d9645c7134086d9b0469278d7bdbfdfc
Ben Walker <bwalke05@gmail.com>
2018-08-21 14:48:50 -0400

    Add id to table modal

--- vue-poker/src/components/TableModal.vue
+++ vue-poker/src/components/TableModal.vue
@@ -1,11 +1,12 @@
 <template lang="pug">
-  b-modal(:active.sync="active")
-    .box
-      form(@submit.prevent="submit" novalidate)
-        h2.subtitle.is-3.has-text-grey.is-marginless.is-unselectable {{ titleize }}
-        a.is-unselectable(@click="newTitle") New Title
-        .has-text-right
-          button.button.is-light(type="submit") Create Table
+  #tableModal
+    b-modal(:active.sync="active")
+      .box
+        form(@submit.prevent="submit" novalidate)
+          h2.subtitle.is-3.has-text-grey.is-marginless.is-unselectable {{ titleize }}
+          a.is-unselectable(@click="newTitle") New Title
+          .has-text-right
+            button.button.is-light(type="submit") Create Table
 </template>
 
 <script>
