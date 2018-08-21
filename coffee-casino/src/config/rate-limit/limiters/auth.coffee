3ef8199f96886c451b574bb8178d807137f1179f
Ben Walker <bwalke05@gmail.com>
2018-08-21 16:21:47 -0400

    Rewrite in plain js

--- coffee-casino/src/config/rate-limit/limiters/auth.coffee
+++
@@ -1,5 +0,0 @@
-import rateLimit from 'express-rate-limit'
-
-export default new rateLimit
-  windowMs: 1 * 60 * 1000
-  max: 5
---
+++ coffee-casino/src/config/rate-limit/limiters/auth.js
@@ -0,0 +1,6 @@
+import rateLimit from 'express-rate-limit'
+
+export default new rateLimit({
+  windowMs: 1 * 60 * 1000,
+  max: 5
+})
