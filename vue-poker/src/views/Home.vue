65ed5c7738feb76d70176cd78d208b7f65e13cbe
Ben Walker <bwalke05@gmail.com>
2018-08-21 12:51:51 -0400

    Include table create modal on home page

--- vue-poker/src/views/Home.vue
+++ vue-poker/src/views/Home.vue
@@ -1,9 +1,15 @@
 <template lang="pug">
   #home
+    TableModal(:active="true")
 </template>
 
 <script>
+import TableModal from '@/components/TableModal.vue'
+
 export default {
-  name: 'home'
+  name: 'home',
+  components: {
+    TableModal
+  }
 }
 </script>
