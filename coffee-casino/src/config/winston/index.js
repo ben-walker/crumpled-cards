1e9f4c4824e6fddad17b3c2b0b42eb1ab61451e1
Ben Walker <bwalke05@gmail.com>
2018-08-21 16:30:20 -0400

    Rewrite winston logging in js

---
+++ coffee-casino/src/config/winston/index.js
@@ -0,0 +1,29 @@
+import winston from 'winston'
+import fs from 'fs'
+import { LOG_DIR, combinedOptions, errorOptions } from './options'
+
+if (!fs.existsSync(LOG_DIR)) fs.mkdirSync(LOG_DIR)
+
+let logger = winston.createLogger({
+  transports: [
+    new winston.transports.File(errorOptions),
+    new winston.transports.File(combinedOptions)
+  ],
+  exitOnError: false
+})
+
+logger.stream = {
+  write: (msg, encoding) => {
+    logger.info(msg)
+  }
+}
+
+const consoleTransport = new winston.transports.Console({
+  format: winston.format.simple()
+})
+
+if (process.env.NODE_ENV !== 'production') {
+  logger.add(consoleTransport)
+}
+
+export default logger
