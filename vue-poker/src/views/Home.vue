e2c69c71600e1f25ea6473bdc6fb1b888239b65a
Ben Walker <bwalke05@gmail.com>
2018-08-21 15:46:12 -0400

    Add navbar to home view

--- vue-poker/src/views/Home.vue
+++ vue-poker/src/views/Home.vue
@@ -1,15 +1,18 @@
 <template lang="pug">
   #home
+    Navbar
     TableModal(:active="true")
 </template>
 
 <script>
+import Navbar from '@/components/Navbar.vue'
 import TableModal from '@/components/TableModal.vue'
 
 export default {
   name: 'home',
   components: {
-    TableModal
+    TableModal,
+    Navbar
   }
 }
 </script>
