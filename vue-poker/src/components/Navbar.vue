2a486c103b46ea99c30e252f942a54d6eab70865
Ben Walker <bwalke05@gmail.com>
2018-08-21 15:46:42 -0400

    Universal app navigation

---
+++ vue-poker/src/components/Navbar.vue
@@ -0,0 +1,19 @@
+<template lang="pug">
+  #navbar
+    nav.navbar.has-shadow.is-spaced
+      .container
+        .navbar-brand
+          TitleHead
+        .navbar-menu
+</template>
+
+<script>
+import TitleHead from '@/components/TitleHead.vue'
+
+export default {
+  name: 'navbar',
+  components: {
+    TitleHead
+  }
+}
+</script>
