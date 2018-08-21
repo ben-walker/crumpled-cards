3c76700b8753ea452cc2141dcba4ed32ac28827e
Ben Walker <bwalke05@gmail.com>
2018-08-21 16:18:43 -0400

    passport rewrite with js

---
+++ coffee-casino/src/config/passport/index.js
@@ -0,0 +1,18 @@
+import localLogin from './strategies/local'
+import { user } from '../../models'
+
+export default (passport) => {
+
+  passport.serializeUser((user, done) => {
+    done(null, user.id)
+  })
+
+  passport.deserializeUser((id, done) => {
+    user.findById(id, '-password', (err, user) => {
+      done(err, user)
+    })
+  })
+
+  passport.use(localLogin)
+
+}
---
+++ coffee-casino/src/config/passport/strategies/local.js
@@ -0,0 +1,23 @@
+import local from 'passport-local'
+import { user } from '../../../models'
+
+localStrategy = local.Strategy
+
+const localLogin = new localStrategy({
+  usernameField: 'identifier'
+},
+function(identifier, password, done) {
+  user.findOne({
+    $or: [
+      { username: identifier },
+      { email: identifier }
+    ] 
+  }, (err, user) => {
+    if (err) return done(err)
+    if (!user) return done(null, false)
+    if (!user.comparePassword(password)) return done(null, false)
+    return done(null, user)
+  })
+})
+
+export default localLogin
