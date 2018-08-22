e440c1dcf36991f0455f52da290ef9b5f70107ca
Ben Walker <bwalke05@gmail.com>
2018-08-22 14:33:55 -0400

    Export all gulp tasks

---
+++ coffee-casino/gulp/tasks/index.js
@@ -0,0 +1,11 @@
+const babel = require('./babel');
+const clean = require('./clean');
+const eslint = require('./eslint');
+const nodemon = require('./nodemon');
+
+module.exports = {
+  babel,
+  clean,
+  eslint,
+  nodemon,
+};
