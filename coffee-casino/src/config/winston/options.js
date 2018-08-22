c95eae792b271e17913a4a2343cf0c426ec99b29
Ben Walker <bwalke05@gmail.com>
2018-08-22 16:37:02 -0400

    Fix log dir creation

--- coffee-casino/src/config/winston/options.js
+++ coffee-casino/src/config/winston/options.js
@@ -1,19 +1,19 @@
 import appRoot from 'app-root-path';
 
-export const LOG_DIR = 'logs';
+export const LOG_DIR = `${appRoot}/logs`;
 const FILE_SIZE = 5242880;
 const MAX_FILES = 5;
 
 export const errorOptions = {
   level: 'error',
-  filename: `${appRoot}/${LOG_DIR}/error.log`,
+  filename: `${LOG_DIR}/error.log`,
   handleExceptions: true,
   maxsize: FILE_SIZE,
   maxFiles: MAX_FILES,
 };
 
 export const combinedOptions = {
-  filename: `${appRoot}/${LOG_DIR}/combined.log`,
+  filename: `${LOG_DIR}/combined.log`,
   handleExceptions: true,
   maxsize: FILE_SIZE,
   maxFiles: MAX_FILES,
