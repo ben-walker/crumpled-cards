291c64dbc7afb8370f6660a6d18e7b9e1300102f
Ben Walker <bwalke05@gmail.com>
2018-08-22 14:10:57 -0400

    Fix eslint erros

--- coffee-casino/src/controllers/table.js
+++ coffee-casino/src/controllers/table.js
@@ -1,15 +1,15 @@
 import { checkSchema, validationResult } from 'express-validator/check';
-import { table, tableValidation } from '../models/table';
+import { Table, tableValidation } from '../models/table';
 
-export const create = [
+export default [
   checkSchema(tableValidation),
-  (req, res, next) => {
+  (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) return res.status(422).json({ errors: errors.array() });
 
-    table.create(req.body, (err, table) => {
+    return Table.create(req.body, (err, table) => {
       if (err) return res.status(500).send('Table creation failure');
-      res.status(200).send(table);
+      return res.status(200).send(table);
     });
   },
 ];
