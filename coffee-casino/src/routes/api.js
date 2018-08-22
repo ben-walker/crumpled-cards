fe63bdc46d09a76a5a8f6bc497597b249f8b0980
Ben Walker <bwalke05@gmail.com>
2018-08-22 14:20:12 -0400

    Fix eslint errors

--- coffee-casino/src/routes/api.js
+++ coffee-casino/src/routes/api.js
@@ -1,9 +1,9 @@
 import express from 'express';
 import * as authController from '../controllers/auth';
 import * as queryController from '../controllers/query';
-import * as meController from '../controllers/me';
-import * as tableController from '../controllers/table';
-import { authLimiter } from '../config/rate-limit';
+import getUserInfo from '../controllers/me';
+import create from '../controllers/table';
+import authLimiter from '../config/rate-limit';
 
 const router = express.Router();
 
@@ -11,13 +11,13 @@ router.post('/register', authLimiter, authController.register);
 router.post('/login', authLimiter, authController.login);
 router.post('/logout', authController.logout);
 
-router.get('/me', meController.getUserInfo);
+router.get('/me', getUserInfo);
 
 router.get('/usernameRegistered', queryController.usernameRegistered);
 router.get('/emailRegistered', queryController.emailRegistered);
 router.get('/identifierExists', queryController.identifierExists);
 router.post('/passwordMatches', authLimiter, queryController.passwordMatches);
 
-router.post('/createTable', tableController.create);
+router.post('/createTable', create);
 
 export default router;
