96a5ed668ecee2eae6cb76eb2fb85b2b27533076
Ben Walker <bwalke05@gmail.com>
2018-08-22 13:55:54 -0400

    Fix eslint errors

--- coffee-casino/src/models/table/index.js
+++ coffee-casino/src/models/table/index.js
@@ -2,11 +2,11 @@ import mongoose from 'mongoose';
 import schema from './schema';
 import validation from './validation';
 
-tableSchema = new mongoose.Schema(schema, {
+const tableSchema = new mongoose.Schema(schema, {
   timestamps: true,
 });
 
-table = mongoose.model('table', tableSchema);
+const table = mongoose.model('table', tableSchema);
 
 export {
   table,
