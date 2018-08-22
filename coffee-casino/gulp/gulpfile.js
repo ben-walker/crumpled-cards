6022b387af65834dce55042298a0664dc847ab1e
Ben Walker <bwalke05@gmail.com>
2018-08-22 14:34:25 -0400

    Remove automatic gulp task compilation

--- coffee-casino/gulp/gulpfile.js
+++ coffee-casino/gulp/gulpfile.js
@@ -1,14 +1,22 @@
 const gulp = require('gulp');
-const utilities = require('./utilities');
 const config = require('./config');
+const {
+  babel,
+  clean,
+  eslint,
+  nodemon,
+} = require('./tasks');
 
-utilities.loadTasks(gulp);
+gulp.task('babel', babel);
+gulp.task('clean', clean);
+gulp.task('eslint', eslint);
+gulp.task('nodemon', nodemon);
 
-gulp.task('build', gulp.series('eslint', 'babel'));
-gulp.task('watch', (done) => {
-  gulp.watch(config.globs.src_files, gulp.series('build'));
+gulp.task('Build', gulp.series('eslint', 'babel'));
+gulp.task('Watch', (done) => {
+  gulp.watch(config.globs.src_files, gulp.series('Build'));
   done();
 });
 
-gulp.task('default', gulp.series('clean', 'build', 'watch', 'nodemon'));
-gulp.task('production', gulp.series('clean', 'build'));
+gulp.task('default', gulp.series('clean', 'Build', 'Watch', 'nodemon'));
+gulp.task('production', gulp.series('clean', 'Build'));
