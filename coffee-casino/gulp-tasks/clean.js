295704e37f0f07335352046981aeded643fd8b1d
Ben Walker <bwalke05@gmail.com>
2018-08-22 09:59:55 -0400

    Fix issue caused by not signalling task completion

--- coffee-casino/gulp-tasks/clean.js
+++ coffee-casino/gulp-tasks/clean.js
@@ -1,9 +1,10 @@
 module.exports = (gulp, plugins, config) => {
   const PATHS = config.paths
-  return () => {
+  return (done) => {
     gulp.src(PATHS.build_dir, {
       read: false,
       allowEmpty: true
     }).pipe(plugins.clean())
+    done()
   }
 }
