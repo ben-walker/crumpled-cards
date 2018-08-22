3c5c1b428ab0b9a062eb14ee204287fe5a799b52
Ben Walker <bwalke05@gmail.com>
2018-08-22 13:55:21 -0400

    Fix eslint errors

--- coffee-casino/src/models/user/index.js
+++ coffee-casino/src/models/user/index.js
@@ -5,21 +5,23 @@ import validation from './validation';
 
 const SALT_WORK = 12;
 
-userSchema = new mongoose.Schema(schema, {
+const userSchema = new mongoose.Schema(schema, {
   timestamps: true,
 });
 
-userSchema.methods.comparePassword = function (candidate, cb) {
+/* eslint-disable func-names */
+userSchema.methods.comparePassword = function (candidate) {
   return bcrypt.compareSync(candidate, this.password);
 };
 
 userSchema.pre('save', function (next) {
   if (!this.isModified('password')) return next();
   this.password = bcrypt.hashSync(this.password, SALT_WORK);
-  next();
+  return next();
 });
+/* eslint-enable */
 
-user = mongoose.model('user', userSchema);
+const user = mongoose.model('user', userSchema);
 
 export {
   user,
