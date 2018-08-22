b24dc9b7185ff6a87fdb457c2a1220954ca3f6dc
Ben Walker <bwalke05@gmail.com>
2018-08-22 10:59:01 -0400

    Add task to babelify code

--- coffee-casino/gulp/tasks/babel.js
+++ coffee-casino/gulp/tasks/babel.js
@@ -3,14 +3,14 @@ const plugins = require('../plugins')
 const config = require('../config')
 
 module.exports = () => {
-  return () => {
+  return (done) => {
     gulp.src(config.globs.src_files)
-      .pipe(plugins.sourcemaps.init({ loadMaps: true }))
-      .pipe(plugins.coffee({ bare: true }))
+      .pipe(plugins.sourcemaps.init())
       .pipe(plugins.babel({
         presets: ['env']
       }))
       .pipe(plugins.sourcemaps.write('./'))
       .pipe(gulp.dest(config.paths.build_dir))
+    done()
   }
 }
