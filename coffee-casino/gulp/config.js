18dee1b41d7a52456ba47511197e6e57f5ef690c
Ben Walker <bwalke05@gmail.com>
2018-08-22 14:26:33 -0400

    Fix eslint errors

--- coffee-casino/gulp/config.js
+++ coffee-casino/gulp/config.js
@@ -2,9 +2,9 @@ module.exports = {
   paths: {
     task_dir: './tasks/',
     build_dir: '../dist',
-    node_entry: '../dist/server.js'
+    node_entry: '../dist/server.js',
   },
   globs: {
-    src_files: '../src/**/*.js'
-  }
-}
+    src_files: '../src/**/*.js',
+  },
+};
