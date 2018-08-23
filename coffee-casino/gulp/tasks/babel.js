316d886f3c432a787d619655714c34d946d312e7
Ben Walker <bwalke05@gmail.com>
2018-08-23 12:16:22 -0400

    Write sourcemaps to subdirectory

--- coffee-casino/gulp/tasks/babel.js
+++ coffee-casino/gulp/tasks/babel.js
@@ -4,9 +4,9 @@ import config from '../config';
 
 export default () => gulp.src(config.globs.src_files)
   .pipe(plugins.changed(config.paths.build_dir))
-  .pipe(plugins.sourcemaps.init({ loadMaps: true }))
+  .pipe(plugins.sourcemaps.init())
   .pipe(plugins.babel({
     presets: ['env'],
   }))
-  .pipe(plugins.sourcemaps.write('./'))
+  .pipe(plugins.sourcemaps.write('./maps'))
   .pipe(gulp.dest(config.paths.build_dir));
