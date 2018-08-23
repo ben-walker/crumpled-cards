4a843e28c9c68df489aebe542cbc5383bcee70af
Ben Walker <bwalke05@gmail.com>
2018-08-23 10:16:09 -0400

    Convert to es6

--- coffee-casino/gulp/tasks/babel.js
+++ coffee-casino/gulp/tasks/babel.js
@@ -1,8 +1,8 @@
-const gulp = require('gulp');
-const plugins = require('../plugins');
-const config = require('../config');
+import gulp from 'gulp';
+import plugins from '../plugins';
+import config from '../config';
 
-module.exports = () => gulp.src(config.globs.src_files)
+export default () => gulp.src(config.globs.src_files)
   .pipe(plugins.changed(config.paths.build_dir))
   .pipe(plugins.sourcemaps.init({ loadMaps: true }))
   .pipe(plugins.babel({
