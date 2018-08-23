951eb211ced5d00ad69672458ac639343ec81b3d
Ben Walker <bwalke05@gmail.com>
2018-08-23 17:19:48 -0400

    Use server utilities

--- coffee-casino/src/server.js
+++ coffee-casino/src/server.js
@@ -2,19 +2,14 @@ import http from 'http';
 import socketIo from 'socket.io';
 import mongoAdapter from 'socket.io-adapter-mongo';
 import app from './app';
-
-function normalizePort(val) {
-  const port = parseInt(val, 10);
-  if (Number.isNaN(Number(port))) return val;
-  if (port >= 0) return port;
-  return val;
-}
+import { normalizePort, onListening } from './server-utils';
 
 const PORT = normalizePort(process.env.PORT || '3000');
 app.set('port', PORT);
 
 const server = http.createServer(app);
 server.listen(PORT);
+server.on('listening', onListening(server));
 
 export const io = socketIo(server, {
   serveClient: false,
