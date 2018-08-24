140461514076ddfa640a21632aee1b88bce661fc
Ben Walker <bwalke05@gmail.com>
2018-08-23 21:03:17 -0400

    Add error listener

--- coffee-casino/src/server.js
+++ coffee-casino/src/server.js
@@ -2,14 +2,16 @@ import http from 'http';
 import socketIo from 'socket.io';
 import mongoAdapter from 'socket.io-adapter-mongo';
 import app from './app';
-import { normalizePort, onListening } from './server-utils';
+import { normalizePort, onListening, onError } from './server-utils';
 
 const PORT = normalizePort(process.env.PORT || '3000');
 app.set('port', PORT);
 
 const server = http.createServer(app);
 server.listen(PORT);
+
 server.on('listening', onListening(server));
+server.on('error', onError(PORT));
 
 export const io = socketIo(server, {
   serveClient: false,
