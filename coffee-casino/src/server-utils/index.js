74755c435304b88380238da3173be07e1a260e8c
Ben Walker <bwalke05@gmail.com>
2018-08-23 20:57:57 -0400

    Export onError

--- coffee-casino/src/server-utils/index.js
+++ coffee-casino/src/server-utils/index.js
@@ -1,7 +1,9 @@
 import normalizePort from './normalize-port';
 import onListening from './on-listening';
+import onError from './on-error';
 
 export {
   normalizePort,
   onListening,
+  onError,
 };
