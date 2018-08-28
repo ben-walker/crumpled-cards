e21e8731fe568285c5234b2e56083c0a9e012a0f
Ben Walker <bwalke05@gmail.com>
2018-08-28 14:50:29 -0400

    Remove all trace of passwordMatches validator

--- vue-poker/src/config/vuelidate-error-extractor/messages.js
+++ vue-poker/src/config/vuelidate-error-extractor/messages.js
@@ -5,7 +5,6 @@ export default {
   maxLength: '{attribute} is too long',
   unique: '{attribute} is already taken',
   email: 'Email is invalid',
-  passwordMatches: 'Password does not match',
   minEntropy: '{attribute} is not strong enough',
   manualError: `If you're seeing this, supply a local error message`
 }
--- vue-poker/src/validators/index.js
+++ vue-poker/src/validators/index.js
@@ -1,4 +1,3 @@
-import passwordMatches from './async/passwordMatches'
 import unique from './async/unique'
 import userExists from './async/userExists'
 
@@ -6,7 +5,6 @@ import manualError from './sync/manualError'
 import minEntropy from './sync/minEntropy'
 
 export {
-  passwordMatches,
   unique,
   userExists,
   manualError,
