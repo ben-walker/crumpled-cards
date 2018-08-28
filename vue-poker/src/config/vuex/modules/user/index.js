5043e4c1d2d6c46d2a7f968962b9578b94a60e7b
Ben Walker <bwalke05@gmail.com>
2018-08-28 14:05:32 -0400

    Use initial state function

--- vue-poker/src/config/vuex/modules/user/index.js
+++ vue-poker/src/config/vuex/modules/user/index.js
@@ -1,12 +1,7 @@
+import state from './state'
 import getters from './getters'
 import actions from './actions'
 import mutations from './mutations'
-
-const state = () => ({
-  authenticated: false,
-  loading: false,
-  data: {}
-})
 
 export default {
   namespaced: true,
