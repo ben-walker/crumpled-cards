362d98477c0e1be54359fcf774f18ad1228b2fb9
Ben Walker <bwalke05@gmail.com>
2018-08-28 14:06:06 -0400

    Add mutation to reset vuex state

--- vue-poker/src/config/vuex/modules/user/mutations.js
+++ vue-poker/src/config/vuex/modules/user/mutations.js
@@ -1,3 +1,6 @@
+import initialState from './state'
+
+export const RESET = 'RESET'
 export const AUTHENTICATE = 'AUTHENTICATE'
 export const REVOKE_AUTH = 'REVOKE_AUTH'
 export const START_LOADING = 'START_LOADING'
@@ -8,8 +11,9 @@ export default {
   [AUTHENTICATE] (state) {
     state.authenticated = true
   },
-  [REVOKE_AUTH] (state) {
-    state.authenticated = false
+  [RESET] (state) {
+    const initial = initialState()
+    Object.keys(initial).forEach(key => { state[key] = initial[key] })
   },
   [START_LOADING] (state) {
     state.loading = true
