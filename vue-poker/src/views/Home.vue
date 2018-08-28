07a05c94a97c87cb25c65cdb4feb881bab71d891
Ben Walker <bwalke05@gmail.com>
2018-08-28 13:31:02 -0400

    Remove Table Modal from home view

--- vue-poker/src/views/Home.vue
+++ vue-poker/src/views/Home.vue
@@ -1,17 +1,14 @@
 <template lang="pug">
   #home
     Navbar
-    TableModal(:active="true")
 </template>
 
 <script>
 import Navbar from '@/components/Navbar.vue'
-import TableModal from '@/components/TableModal.vue'
 
 export default {
   name: 'home',
   components: {
-    TableModal,
     Navbar
   }
 }
