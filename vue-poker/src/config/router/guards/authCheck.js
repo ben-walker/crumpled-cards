7c505f964bdd6c7059b8e18535b358854a49afdd
Ben Walker <bwalke05@gmail.com>
2018-08-28 10:21:19 -0400

    Use error from getMe action to determine auth status

--- vue-poker/src/config/router/guards/authCheck.js
+++ vue-poker/src/config/router/guards/authCheck.js
@@ -1,9 +1,10 @@
+import toAsync from 'await-to-js'
 import store from '@/config/vuex'
 
 export default async (to, from, next) => {
-  await store.dispatch('user/getMe')
+  const [ err ] = await toAsync(store.dispatch('user/getMe'))
   const authRequired = to.matched.some((route) => route.meta.auth)
-  const authenticated = store.state.user.authenticated
+  const authenticated = err === null
 
   if (authRequired && !authenticated) next('/welcome')
   else if (!authRequired && authenticated) next('/')
