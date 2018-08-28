7221d412ddc05fea43fb2172432f76a5e7490d2e
Ben Walker <bwalke05@gmail.com>
2018-08-28 13:48:24 -0400

    Replace user state declaration with function returning initial state

--- vue-poker/src/config/vuex/modules/user/index.js
+++ vue-poker/src/config/vuex/modules/user/index.js
@@ -2,15 +2,15 @@ import getters from './getters'
 import actions from './actions'
 import mutations from './mutations'
 
-const state = {
+const state = () => ({
   authenticated: false,
   loading: false,
   data: {}
-}
+})
 
 export default {
   namespaced: true,
-  state,
+  state: state(),
   getters,
   actions,
   mutations
