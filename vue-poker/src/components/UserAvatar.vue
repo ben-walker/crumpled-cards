127fe2eaf667c72dbedd072884f175bf78395225
Ben Walker <bwalke05@gmail.com>
2018-08-29 15:44:54 -0400

    Display profile picture from server

--- vue-poker/src/components/UserAvatar.vue
+++ vue-poker/src/components/UserAvatar.vue
@@ -1,6 +1,6 @@
 <template lang="pug">
   .userAvatar
-    avatar.is-unselectable(:size="size" color="white" :username="username")
+    avatar.is-unselectable(:size="size" color="white" :username="username" :src="profilePictureURL")
 </template>
 
 <script>
@@ -10,7 +10,8 @@ export default {
   name: 'userAvatar',
   data () {
     return {
-      username: this.$store.getters['user/username']
+      username: this.$store.getters['user/username'],
+      profilePictureURL: `${process.env.VUE_APP_API}profilePicture/${this.$store.getters['user/id']}`
     }
   },
   props: {
