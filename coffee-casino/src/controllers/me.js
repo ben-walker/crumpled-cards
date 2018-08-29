186312ca8ce8cb331898596f0d4856eb91066257
Ben Walker <bwalke05@gmail.com>
2018-08-29 14:36:47 -0400

    Create a separate profile controller

--- coffee-casino/src/controllers/me.js
+++ coffee-casino/src/controllers/me.js
@@ -1,8 +1,4 @@
-export const getUserInfo = (req, res) => {
+export default (req, res) => {
   if (req.isAuthenticated()) res.status(200).send({ user: req.user });
   else res.status(404).send({ user: null });
-};
-
-export const uploadProfilePic = () => {
-
 };
