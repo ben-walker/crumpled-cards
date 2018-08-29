56b10b3ff0f027ddba825f478b00049d65a650f1
Ben Walker <bwalke05@gmail.com>
2018-08-29 15:10:35 -0400

    Endpoint to handle profile pic uploads and save em to mongo

--- coffee-casino/src/controllers/profile.js
+++ coffee-casino/src/controllers/profile.js
@@ -1,3 +1,19 @@
-export default (req, res) => {
+import { ProfilePicture } from '../models/profile-picture';
 
+export default (req, res) => {
+  ProfilePicture.findOneAndUpdate(
+    { user: req.user },
+    {
+      data: req.files.profilePicture.data,
+      mimetype: req.files.profilePicture.mimetype,
+    },
+    {
+      upsert: true,
+      new: true,
+    },
+    (err, profilePicture) => {
+      if (err) return res.status(500).send('Profile picture upload failed');
+      return res.status(200).send(profilePicture);
+    },
+  );
 };
