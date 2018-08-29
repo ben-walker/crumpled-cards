37e631aa886f5428a2ac05e865dd8e83a0964e07
Ben Walker <bwalke05@gmail.com>
2018-08-29 11:12:47 -0400

    Reusable component for user's avatar

---
+++ vue-poker/src/components/UserAvatar.vue
@@ -0,0 +1,26 @@
+<template lang="pug">
+  .userAvatar
+    avatar.is-unselectable(:size="size" color="white" :username="username")
+</template>
+
+<script>
+import Avatar from 'vue-avatar'
+
+export default {
+  name: 'userAvatar',
+  data () {
+    return {
+      username: this.$store.getters['user/username']
+    }
+  },
+  props: {
+    size: {
+      default: 50,
+      type: Number
+    }
+  },
+  components: {
+    Avatar
+  }
+}
+</script>
