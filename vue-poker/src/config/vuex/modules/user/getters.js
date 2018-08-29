d77addb6e6f24c52c98de0800b361587eb911522
Ben Walker <bwalke05@gmail.com>
2018-08-29 15:39:01 -0400

    ID getter

--- vue-poker/src/config/vuex/modules/user/getters.js
+++ vue-poker/src/config/vuex/modules/user/getters.js
@@ -1,5 +1,6 @@
 export default {
   authenticated: state => !!state.authenticated,
   username: state => state.data.username ? state.data.username : '',
-  email: state => state.data.email ? state.data.email : ''
+  email: state => state.data.email ? state.data.email : '',
+  id: state => state.data._id ? state.data._id : ''
 }
