39f86fe8ea132bed59a3d6e717619f5c29d90edd
Ben Walker <bwalke05@gmail.com>
2018-08-29 11:33:12 -0400

    Show username/email on profile modal

--- vue-poker/src/components/ProfileModal.vue
+++ vue-poker/src/components/ProfileModal.vue
@@ -4,6 +4,10 @@
       .columns.is-centered
         .column.is-narrow
           user-avatar(:size="170")
+        .column.is-narrow
+          br
+          h1.title.is-4 {{ username }}
+          h2.subtitle.is-6 {{ email }}
 </template>
 
 <script>
@@ -13,7 +17,8 @@ export default {
   name: 'profileModal',
   data () {
     return {
-      username: this.$store.getters['user/username']
+      username: this.$store.getters['user/username'],
+      email: this.$store.getters['user/email']
     }
   },
   components: {
--- vue-poker/src/config/vuex/modules/user/getters.js
+++ vue-poker/src/config/vuex/modules/user/getters.js
@@ -1,4 +1,5 @@
 export default {
   authenticated: state => !!state.authenticated,
-  username: state => state.data.username ? state.data.username : ''
+  username: state => state.data.username ? state.data.username : '',
+  email: state => state.data.email ? state.data.email : ''
 }
