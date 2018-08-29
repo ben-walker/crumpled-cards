0bce52fbdca2e0daebd5f1dde1572e969902a3d1
Ben Walker <bwalke05@gmail.com>
2018-08-29 11:24:48 -0400

    Display avatar on profile

--- vue-poker/src/components/ProfileModal.vue
+++ vue-poker/src/components/ProfileModal.vue
@@ -1,10 +1,23 @@
 <template lang="pug">
   #profileModal
     .box
+      .columns.is-centered
+        .column.is-narrow
+          user-avatar(:size="170")
 </template>
 
 <script>
+import UserAvatar from '@/components/UserAvatar.vue'
+
 export default {
-  name: 'profileModal'
+  name: 'profileModal',
+  data () {
+    return {
+      username: this.$store.getters['user/username']
+    }
+  },
+  components: {
+    UserAvatar
+  }
 }
 </script>
