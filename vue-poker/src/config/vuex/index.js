6ed575db8ffd20a6340952b99f27fdcb22f01f6b
Ben Walker <bwalke05@gmail.com>
2018-08-30 09:51:09 -0400

    Use reducer to determine what vuex data should be persisted to local storage

--- vue-poker/src/config/vuex/index.js
+++ vue-poker/src/config/vuex/index.js
@@ -7,11 +7,18 @@ Vue.use(Vuex)
 
 const debug = process.env.NODE_ENV !== 'production'
 
+const reducer = state => ({
+  user: state.user,
+  socket: state.socket
+})
+
 export default new Vuex.Store({
   modules: {
     user,
     socket
   },
-  plugins: [createPersistedState()],
+  plugins: [
+    createPersistedState({ reducer })
+  ],
   strict: debug
 })
