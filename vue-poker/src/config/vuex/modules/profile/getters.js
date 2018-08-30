2bc029a88650a1b534770c836ed14fb279898f52
Ben Walker <bwalke05@gmail.com>
2018-08-30 16:50:24 -0400

    Provide getter for profile picture url

--- vue-poker/src/config/vuex/modules/profile/getters.js
+++ vue-poker/src/config/vuex/modules/profile/getters.js
@@ -1,5 +1,6 @@
 export default {
   id: state => state.id,
   username: state => state.username,
-  email: state => state.email
+  email: state => state.email,
+  pictureRef: state => `${process.env.VUE_APP_API}profilePicture/${state.id}`
 }
