2475507cfc8c7ba6e016eed77f31555956a4eac4
Ben Walker <bwalke05@gmail.com>
2018-08-30 11:31:27 -0400

    Use new vuex-connect enabled user dropdown component

--- vue-poker/src/components/Navbar.vue
+++ vue-poker/src/components/Navbar.vue
@@ -7,45 +7,22 @@
         .navbar-menu
           .navbar-start
           .navbar-end
-            .navbar-item.has-dropdown.is-hoverable
-              a #[user-avatar]
-              .navbar-dropdown.is-boxed.is-right
-                .navbar-item
-                  strong.is-unselectable {{ username }}
-                hr.navbar-divider
-                a.navbar-item(@click="openProfile")
-                  span.icon.has-text-primary
-                    i.fas.fa-user
-                  strong.is-unselectable Profile
-                a.navbar-item(@click="logout")
-                  span.icon.has-text-danger
-                    i.fas.fa-sign-out-alt
-                  strong.is-unselectable Log Out
+            user-dropdown(@openProfile="openProfile")
 </template>
 
 <script>
-import to from 'await-to-js'
 import TitleHead from '@/components/TitleHead.vue'
 import ProfileModal from '@/components/ProfileModal.vue'
-import UserAvatar from '@/components/UserAvatar.vue'
+import UserDropdown from '@/components/UserDropdown'
 
 export default {
   name: 'navbar',
-  data () {
-    return {
-      username: this.$store.getters['user/username']
-    }
-  },
   components: {
     TitleHead,
     ProfileModal,
-    UserAvatar
+    UserDropdown
   },
   methods: {
-    async logout () {
-      const [ err ] = await to(this.$store.dispatch('user/logOut'))
-      if (!err) this.$router.push('/welcome')
-    },
     openProfile () {
       this.$modal.open({
         parent: this,
