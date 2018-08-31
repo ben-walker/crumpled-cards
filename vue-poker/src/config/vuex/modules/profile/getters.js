d21c18d01a79fa8fc5f6d27566ff738f0b6cb3f4
Ben Walker <bwalke05@gmail.com>
2018-08-31 11:03:49 -0400

    Hold profilePicUrl in profile state, access via getter

--- vue-poker/src/config/vuex/modules/profile/getters.js
+++ vue-poker/src/config/vuex/modules/profile/getters.js
@@ -2,5 +2,5 @@ export default {
   id: state => state.id,
   username: state => state.username,
   email: state => state.email,
-  pictureRef: state => `${process.env.VUE_APP_API}profilePicture/${state.id}`
+  pictureRef: state => state.profilePicUrl
 }
--- vue-poker/src/config/vuex/modules/profile/state.js
+++ vue-poker/src/config/vuex/modules/profile/state.js
@@ -1,5 +1,6 @@
 export default () => ({
   id: '',
   username: '',
-  email: ''
+  email: '',
+  profilePicUrl: ''
 })
