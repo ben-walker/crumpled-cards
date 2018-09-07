081c4dab97ba0f48535a2372c60eed4a08152d03
Ben Walker <bwalke05@gmail.com>
2018-09-07 14:34:20 -0400

    Import only required buefy components

--- vue-poker/src/config/buefy/index.js
+++ vue-poker/src/config/buefy/index.js
@@ -1,13 +1,11 @@
 import Vue from 'vue'
-import Buefy from 'buefy'
-
-Vue.use(Buefy, { defaultIconPack: 'fas' })
-/* import config from 'buefy/src/utils/config'
-import { Field, Input, Icon, Toast, Modal } from 'buefy/dist/components'
+import config from 'buefy/src/utils/config'
+import { Field, Input, Icon, Toast, Modal, Upload } from 'buefy/dist/components'
 
 Vue.use(Field)
 Vue.use(Input)
 Vue.use(Icon)
 Vue.use(Toast)
 Vue.use(Modal)
-config.defaultIconPack = 'fas' */
+Vue.use(Upload)
+config.defaultIconPack = 'fas'
