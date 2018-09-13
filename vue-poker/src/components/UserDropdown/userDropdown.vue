661a7fbc8c316e46fe2f278abce5d0bf3e992fbc
Ben Walker <bwalke05@gmail.com>
2018-09-13 13:17:52 -0400

    Simplify user dropdown by using buefy dropdown component

--- vue-poker/src/components/UserDropdown/userDropdown.vue
+++ vue-poker/src/components/UserDropdown/userDropdown.vue
@@ -1,22 +1,21 @@
 <template lang="pug">
   #userDropdown
-    .navbar-item.has-dropdown(:class="{ 'is-active': isActive }" v-on-clickaway="hide")
-      a(@click="toggle") #[user-avatar]
-      .navbar-dropdown.is-boxed.is-right
-        a.navbar-item(@click="openProfile")
-          span
-            .columns.is-centered
-              .column.is-narrow
-                user-avatar
-              .column.is-narrow
-                h1.title.is-4.is-unselectable {{ username }}
-                h2.subtitle.is-6.is-unselectable {{ email }}
+    b-dropdown(position="is-bottom-left")
+      a(slot="trigger") #[user-avatar]
+      b-dropdown-item(@click="openProfile")
+        span
+          .columns.is-centered
+            .column.is-narrow
+              user-avatar
+            .column.is-narrow
+              h1.title.is-4.is-unselectable {{ username }}
+              h2.subtitle.is-6.is-unselectable {{ email }}
 
-        hr.navbar-divider
+      hr.dropdown-divider
 
-        a.navbar-item(@click="logOut")
-          nav.level.is-mobile
-            .level-left
+      b-dropdown-item(@click="logOut")
+        nav.level.is-mobile
+          .level-left
               b-icon.has-text-danger.level-item(pack="fas" icon="sign-out-alt")
               strong.title.is-6.is-unselectable.level-item Log Out
 </template>
@@ -24,15 +23,9 @@
 <script>
 import UserAvatar from '@/components/UserAvatar'
 import Profile from '@/components/Profile'
-import { mixin as clickaway } from 'vue-clickaway'
 
 export default {
   name: 'userDropdown',
-  data () {
-    return {
-      isActive: false
-    }
-  },
   props: {
     username: {
       type: String,
@@ -47,26 +40,15 @@ export default {
     UserAvatar,
     Profile
   },
-  mixins: [
-    clickaway
-  ],
   methods: {
-    toggle () {
-      this.isActive = !this.isActive
-    },
-    hide () {
-      this.isActive = false
-    },
     openProfile () {
       this.$modal.open({
         parent: this,
         component: Profile,
         canCancel: ['x', 'outside']
       })
-      this.hide()
     },
     logOut () {
-      this.hide()
       this.$emit('logOut')
     }
   }
--- vue-poker/src/config/buefy/index.js
+++ vue-poker/src/config/buefy/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import config from 'buefy/src/utils/config'
-import { Field, Input, Icon, Toast, Modal, Upload } from 'buefy/dist/components'
+import { Field, Input, Icon, Toast, Modal, Upload, Dropdown } from 'buefy/dist/components'
 
 Vue.use(Field)
 Vue.use(Input)
@@ -8,4 +8,5 @@ Vue.use(Icon)
 Vue.use(Toast)
 Vue.use(Modal)
 Vue.use(Upload)
+Vue.use(Dropdown)
 config.defaultIconPack = 'fas'
