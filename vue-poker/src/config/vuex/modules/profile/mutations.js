b7b94584d43541489d8c86b0cc41fa58914562f8
Ben Walker <bwalke05@gmail.com>
2018-09-18 17:03:18 -0400

    Remove profile pic update mutation, reorder existing mutations

--- vue-poker/src/config/vuex/modules/profile/mutations.js
+++ vue-poker/src/config/vuex/modules/profile/mutations.js
@@ -5,19 +5,15 @@ export const RESET = 'RESET'
 export const UPDATE_PROFILE_PIC = 'UPDATE_PROFILE_PIC'
 
 export default {
-  [POPULATE] (state, userData) {
-    state.id = userData._id
-    state.username = userData.username
-    state.email = userData.email
-    state.picture = userData.profilePicture
-  },
-
   [RESET] (state) {
     const initial = initialState()
     Object.keys(initial).forEach(key => { state[key] = initial[key] })
   },
 
-  [UPDATE_PROFILE_PIC] (state, profilePicId) {
-    state.profilePicUrl = `${process.env.VUE_APP_API}profilePicture/${profilePicId}`
+  [POPULATE] (state, userData) {
+    state.id = userData._id
+    state.username = userData.username
+    state.email = userData.email
+    state.picture = userData.profilePicture
   }
 }
