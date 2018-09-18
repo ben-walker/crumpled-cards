e2e1f80b7e58708aa2bb90a94af3f000b373b9c5
Ben Walker <bwalke05@gmail.com>
2018-09-17 21:24:41 -0400

    Add functions to deal with profile pictures

--- coffee-casino/src/controllers/user.js
+++ coffee-casino/src/controllers/user.js
@@ -1,4 +1,4 @@
-import { User } from '../models';
+import { User, ProfilePicture } from '../models';
 
 export const getMe = (req, res) => {
   if (req.isAuthenticated()) res.status(200).send({ user: req.user });
@@ -29,5 +29,47 @@ export const identifierExists = (req, res) => {
   }, (err, user) => {
     if (err) res.status(500).send('Request failure');
     else res.status(200).send(!!user);
+  });
+};
+
+export const uploadProfilePic = (req, res) => {
+  if (!req.isAuthenticated()) return res.status(401).send();
+  const { user, files } = req;
+
+  ProfilePicture.findByIdAndRemove(user.profilePicture);
+
+  return ProfilePicture.create({
+    img: files.profilePicture.data,
+    mimetype: files.profilePicture.mimetype,
+  }, (err, profilePicture) => {
+    if (err) return res.status(500).send('Profile picture upload failed');
+    user.profilePicture = profilePicture;
+    return user.save((saveErr) => {
+      if (saveErr) return res.status(500).send('Profile picture upload failed');
+      return res.status(200).send();
+    });
+  });
+};
+
+const findProfilePicByUsername = (req, res) => {
+  const { username } = req.query;
+  User.findProfilePic(username, (err, pic) => {
+    if (err) return res.status(500).send('Profile picture retrieval failed');
+    return ProfilePicture.findById(pic.profilePicture, (picErr, profilePicture) => {
+      if (picErr) return res.status(500).send('Profile picture retrieval failed');
+      if (!profilePicture) return res.status(404).send('Profile picture not found');
+      return res.status(200).send(profilePicture.img);
+    });
+  });
+};
+
+export const getProfilePic = (req, res) => {
+  if (req.query.username) return findProfilePicByUsername(req, res);
+  const { user } = req;
+
+  return ProfilePicture.findById(user.profilePicture, (err, profilePicture) => {
+    if (err) return res.status(500).send('Profile picture retrieval failed');
+    if (!profilePicture) return res.status(404).send('Profile picture not found');
+    return res.status(200).send(profilePicture.img);
   });
 };
