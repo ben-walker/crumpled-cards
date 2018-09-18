8d78b6a225265a80cf787dcaccf0813ba947b956
Ben Walker <bwalke05@gmail.com>
2018-09-18 16:56:12 -0400

    Check that profile pic exists before 'getting' it

--- vue-poker/src/config/vuex/modules/profile/getters.js
+++ vue-poker/src/config/vuex/modules/profile/getters.js
@@ -3,6 +3,7 @@ export default {
   username: state => state.username,
   email: state => state.email,
   picture: state => {
+    if (!state.picture.img) return ''
     let encoding = btoa(String.fromCharCode.apply(null, state.picture.img.data))
     return `data:image/jpg;base64,${encoding}`
   }
