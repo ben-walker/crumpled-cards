aea7543dcae62b096024561d62bfdf24c76bd2f2
Ben Walker <bwalke05@gmail.com>
2018-09-19 14:42:30 -0400

    Include stub user search component on Friends view

--- vue-poker/src/views/Friends.vue
+++ vue-poker/src/views/Friends.vue
@@ -2,10 +2,18 @@
   #friends
     section.section
       .container
+        .columns.is-centered
+          .column.is-one-third
+            user-search
 </template>
 
 <script>
+import { UserSearch } from '@/components'
+
 export default {
-  name: 'friends'
+  name: 'friends',
+  components: {
+    UserSearch
+  }
 }
 </script>
