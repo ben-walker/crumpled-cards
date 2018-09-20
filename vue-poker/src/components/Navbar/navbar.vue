b5c08189dd45ecea4273e45dc1f3874fbd4203ca
Ben Walker <bwalke05@gmail.com>
2018-09-20 19:53:59 -0400

    Change navbar items to raw router-links

--- vue-poker/src/components/Navbar/navbar.vue
+++ vue-poker/src/components/Navbar/navbar.vue
@@ -1,23 +1,21 @@
 <template lang="pug">
   #navbar
-    nav.navbar.has-shadow.is-spaced
+    nav.navbar.has-shadow.is-spaced.is-transparent
       .container
         .navbar-brand
           title-head.navbar-item
         .navbar-menu
           .navbar-start
-            .navbar-item
-              router-link.button.is-dark.is-outlined(:to="{ name: 'tables' }" tag="button")
-                b-icon(pack="fas" icon="layer-group" size="is-small")
-                span Tables
-
-            .navbar-item
-              router-link.button.is-dark.is-outlined(:to="{ name: 'friends' }" tag="button")
-                b-icon(pack="fas" icon="user-friends" size="is-small")
-                span Friends
-
           .navbar-end
-            user-dropdown
+            router-link.navbar-item(:to="{ name: 'tables' }" active-class="is-active")
+              span.icon
+                b-icon(pack="fas" icon="layer-group" size="is-small")
+              span Tables
+            router-link.navbar-item(:to="{ name: 'friends' }" active-class="is-active")
+              span.icon
+                b-icon(pack="fas" icon="user-friends" size="is-small")
+              span Friends
+            user-dropdown.navbar-item
 </template>
 
 <script>
