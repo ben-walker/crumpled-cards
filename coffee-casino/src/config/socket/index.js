c38d06f6cf7783b501f728783891c190222be1e3
Ben Walker <bwalke05@gmail.com>
2018-09-20 20:23:05 -0400

    Make req user available to socket

--- coffee-casino/src/config/socket/index.js
+++ coffee-casino/src/config/socket/index.js
@@ -1,6 +1,9 @@
 import socketIo from 'socket.io';
 import mongoAdapter from 'socket.io-adapter-mongo';
+import passportSocketIo from 'passport.socketio';
+import cookieParser from 'cookie-parser';
 import logger from '../debugger';
+import store from '../store';
 
 export default (server) => {
   const io = socketIo(server, {
@@ -8,7 +11,13 @@ export default (server) => {
     adapter: mongoAdapter(process.env.MONGODB_URI),
   });
 
+  io.use(passportSocketIo.authorize({
+    cookieParser,
+    secret: process.env.SECRET,
+    store,
+  }));
+
   io.on('connection', (socket) => {
-    logger(`Socket connected: ${socket.id}`);
+    logger(`User connected: ${socket.request.user.username}`);
   });
 };
