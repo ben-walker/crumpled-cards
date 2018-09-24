405c3b1379af974af67667c4fb2cfa4a85fb355f
Ben Walker <bwalke05@gmail.com>
2018-09-24 15:45:26 -0400

    Import tooltips

--- vue-poker/src/config/buefy/index.js
+++ vue-poker/src/config/buefy/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import config from 'buefy/src/utils/config'
-import { Field, Input, Icon, Toast, Modal, Upload, Dropdown } from 'buefy/dist/components'
+import { Field, Input, Icon, Toast, Modal, Upload, Dropdown, Tooltip } from 'buefy/dist/components'
 
 Vue.use(Field)
 Vue.use(Input)
@@ -9,4 +9,5 @@ Vue.use(Toast)
 Vue.use(Modal)
 Vue.use(Upload)
 Vue.use(Dropdown)
+Vue.use(Tooltip)
 config.defaultIconPack = 'fas'
