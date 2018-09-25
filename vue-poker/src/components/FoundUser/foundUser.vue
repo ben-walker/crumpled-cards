751f61c463cd853bba4cc4ec3a8dcc9ca510682d
Ben Walker <bwalke05@gmail.com>
2018-09-24 20:08:50 -0400

    Add component for users found when searching

---
+++ vue-poker/src/components/FoundUser/foundUser.vue
@@ -0,0 +1,26 @@
+<template lang="pug">
+  .foundUser
+    .card
+      .card-content.has-text-centered
+        user-avatar(:size="140" :username="user.username" :profilePicture="user.profilePicture")
+        h1.title.is-5.has-text-grey-dark.is-unselectable {{ user.username }}
+      .card-footer
+        a.card-footer-item #[b-icon(pack="fas" icon="plus")]
+</template>
+
+<script>
+import UserAvatar from '@/components/UserAvatar/userAvatar.vue'
+
+export default {
+  name: 'foundUser',
+  props: {
+    user: {
+      type: Object,
+      required: true
+    }
+  },
+  components: {
+    UserAvatar
+  }
+}
+</script>
---
+++ vue-poker/src/components/FoundUser/index.js
@@ -0,0 +1,4 @@
+import { connect } from 'vuex-connect'
+import foundUser from './foundUser.vue'
+
+export default connect({})('foundUser', foundUser)
