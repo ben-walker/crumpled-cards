f1e399a847f328861100c9206c966af612d7a867
Ben Walker <bwalke05@gmail.com>
2018-09-25 16:39:30 -0400

    Provie 'add' button to user badge slot

--- vue-poker/src/components/FoundUser/foundUser.vue
+++ vue-poker/src/components/FoundUser/foundUser.vue
@@ -1,6 +1,8 @@
 <template lang="pug">
   .foundUser.has-text-centered
     user-avatar(:size="140" :username="user.username" :profilePicture="user.profilePicture")
+      button.button.is-primary(slot="user-badge")
+        b-icon(pack="fas" icon="plus" size="is-small")
     h1.title.is-5.has-text-grey-dark.is-unselectable {{ user.username }}
 </template>
 
