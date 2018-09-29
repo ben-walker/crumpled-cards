1d6e5481e740f7d9d2e5df4b82b37fd6d89f2e24
Ben Walker <bwalke05@gmail.com>
2018-09-29 15:28:46 -0400

    Conditionally add and remove has-spaced-navbar-fixed-top class to <html?

--- vue-poker/src/views/Home.vue
+++ vue-poker/src/views/Home.vue
@@ -8,10 +8,20 @@
 <script>
 import { Navbar } from '@/components'
 
+const FIXED_NAV_CLASS = 'has-spaced-navbar-fixed-top'
+
 export default {
   name: 'home',
   components: {
     Navbar
+  },
+  created () {
+    const root = document.documentElement
+    root.classList.add(FIXED_NAV_CLASS)
+  },
+  beforeDestroy () {
+    const root = document.documentElement
+    root.classList.remove(FIXED_NAV_CLASS)
   }
 }
 </script>
