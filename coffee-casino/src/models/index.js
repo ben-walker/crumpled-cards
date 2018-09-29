7d944af8b2a3a188f76e2abfc5372b7a7fe4fe3b
Ben Walker <bwalke05@gmail.com>
2018-09-29 15:50:40 -0400

    Export friend request model

--- coffee-casino/src/models/index.js
+++ coffee-casino/src/models/index.js
@@ -1,9 +1,11 @@
 import { User } from './user';
+import { FriendRequest } from './friendRequest';
 import { Table } from './table';
 import { ProfilePicture } from './profile-picture';
 
 export {
   User,
+  FriendRequest,
   Table,
   ProfilePicture,
 };
