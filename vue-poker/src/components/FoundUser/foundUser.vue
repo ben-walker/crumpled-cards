41119b4dd09f13632fda3e7dc6083ace03fd5bf3
Ben Walker <bwalke05@gmail.com>
2018-09-29 17:39:31 -0400

    Enable POST to /sendFriendRequest

--- vue-poker/src/components/FoundUser/foundUser.vue
+++ vue-poker/src/components/FoundUser/foundUser.vue
@@ -1,11 +1,13 @@
 <template lang="pug">
   .foundUser.has-text-centered
     user-avatar(:size="140" :username="user.username" :profilePicture="user.profilePicture")
-      a(slot="user-badge") #[user-badge(status="add")]
+      a(slot="user-badge" @click="sendRequest(user.username)") #[user-badge(status="add")]
     h1.title.is-5.has-text-grey-dark.is-unselectable {{ user.username }}
 </template>
 
 <script>
+import { httpProgress } from '@/config/axios'
+import to from 'await-to-js'
 import UserAvatar from '@/components/UserAvatar/userAvatar.vue'
 import UserBadge from '@/components/UserBadge/userBadge.vue'
 
@@ -20,6 +22,14 @@ export default {
   components: {
     UserAvatar,
     UserBadge
+  },
+  methods: {
+    async sendRequest (username) {
+      const [ err ] = await to(httpProgress.post('sendFriendRequest', {
+        username
+      }))
+      console.log(err)
+    }
   }
 }
 </script>
