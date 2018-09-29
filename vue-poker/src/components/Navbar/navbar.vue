88d0ef5a05796db9c8757c1041b642f510fb193d
Ben Walker <bwalke05@gmail.com>
2018-09-29 15:26:44 -0400

    Fix navbar to top

--- vue-poker/src/components/Navbar/navbar.vue
+++ vue-poker/src/components/Navbar/navbar.vue
@@ -1,6 +1,6 @@
 <template lang="pug">
   #navbar
-    nav.navbar.has-shadow.is-spaced.is-transparent
+    nav.navbar.has-shadow.is-spaced.is-transparent.is-fixed-top
       .container
         .navbar-brand
           title-head.navbar-item
