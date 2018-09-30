bfaab4ddc6a7cff09a1b8e9d1b2719ee65c45a8b
Ben Walker <bwalke05@gmail.com>
2018-09-29 21:00:11 -0400

    Use async/await for friend request sending

--- coffee-casino/src/controllers/friend.js
+++ coffee-casino/src/controllers/friend.js
@@ -1,23 +1,23 @@
+import to from 'await-to-js';
 import { User, FriendRequest } from '../models';
 
-export const sendRequest = (req, res) => {
+export const sendRequest = async (req, res) => {
   const { user } = req;
   const { username } = req.body;
 
   FriendRequest.findOneAndDelete({ requester: user.username, recipient: username }).exec();
 
-  User.findOne({ username }, (err, recipient) => {
-    if (err) return res.status(500).send('Friend request failed');
-    if (!recipient) return res.status(404).send('Recipient not found');
+  const [err, recipient] = await to(User.findOne({ username }).exec());
+  if (err) return res.status(500).send('Friend request failed');
+  if (!recipient) return res.status(404).send('Recipient not found');
 
-    return FriendRequest.create({
-      requester: user.username,
-      recipient: recipient.username,
-    }, (createErr, friendRequest) => {
-      if (createErr) return res.status(500).send('Friend request failed');
-      return res.status(200).send(friendRequest);
-    });
+  const friendRequest = new FriendRequest({
+    requester: user.username,
+    recipient: recipient.username,
   });
+  const [createErr, doc] = await to(friendRequest.save());
+  if (createErr) return res.status(500).send('Friend request failed');
+  return res.status(200).send(doc);
 };
 
 export const acceptRequest = () => {};
