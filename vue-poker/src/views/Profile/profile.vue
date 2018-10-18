62246fbe5c90257d2b1966c60c5041c3c7dcdef7
Ben Walker <bwalke05@gmail.com>
2018-10-18 15:17:58 -0400

    Define in-route transition for profile view

--- vue-poker/src/views/Profile/profile.vue
+++ vue-poker/src/views/Profile/profile.vue
@@ -1,17 +1,18 @@
 <template lang="pug">
-  #profile
-    section.hero.is-fullheight
-      .hero-head
-        a(@click="$router.go(-1)").has-text-dark.is-pulled-right #[b-icon(pack="fas" icon="times" size="is-large")]
-        .section
-          .container.has-text-centered
-            user-avatar(:size="170")
-            br
-            p.is-size-4.has-text-grey-dark.has-text-weight-semibold.is-unselectable {{ username }}
-            p.is-size-4.has-text-grey-dark.has-text-weight-semibold.is-unselectable {{ email }}
-            button.button.is-danger(@click="logOut")
-              b-icon(pack="fas" icon="sign-out-alt" size="is-small")
-              span Log Out
+  transition(enter-active-class="animated fadeIn")
+    #profile
+      section.hero.is-fullheight
+        .hero-head
+          a(@click="$router.go(-1)").has-text-dark.is-pulled-right #[b-icon(pack="fas" icon="times" size="is-large")]
+          .section
+            .container.has-text-centered
+              user-avatar(:size="170")
+              br
+              p.is-size-4.has-text-grey-dark.has-text-weight-semibold.is-unselectable {{ username }}
+              p.is-size-4.has-text-grey-dark.has-text-weight-semibold.is-unselectable {{ email }}
+              button.button.is-danger(@click="logOut")
+                b-icon(pack="fas" icon="sign-out-alt" size="is-small")
+                span Log Out
 </template>
 
 <script>
