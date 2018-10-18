96bc51e381dfb8f08849d7b75a4a3b6911b9d292
Ben Walker <bwalke05@gmail.com>
2018-10-18 15:49:49 -0400

    Add edit badge to profile

--- vue-poker/src/views/Profile/profile.vue
+++ vue-poker/src/views/Profile/profile.vue
@@ -5,15 +5,18 @@
         .hero-head
           a(@click="$router.go(-1)").has-text-dark.is-pulled-right #[b-icon(pack="fas" icon="times" size="is-large")]
           .section
-            .container.has-text-centered
-              user-avatar(:size="170")
-              br
-              p.is-size-4.has-text-grey-dark.has-text-weight-semibold.is-unselectable {{ username }}
-              p.is-size-4.has-text-grey-dark.has-text-weight-semibold.is-unselectable {{ email }}
+            .columns.is-centered
+              .column.is-narrow.has-text-centered
+                user-avatar(:size="220")
+                  a(slot="user-badge") #[user-badge(status="edit")]
+                br
+                p.is-size-4.has-text-grey-dark.has-text-weight-semibold.is-unselectable {{ username }}
+                p.is-size-4.has-text-grey-dark.has-text-weight-semibold.is-unselectable {{ email }}
 </template>
 
 <script>
 import UserAvatar from '@/components/UserAvatar'
+import UserBadge from '@/components/UserBadge'
 
 export default {
   name: 'profile',
@@ -33,7 +36,8 @@ export default {
     }
   },
   components: {
-    UserAvatar
+    UserAvatar,
+    UserBadge
   },
   methods: {
     upload () {
