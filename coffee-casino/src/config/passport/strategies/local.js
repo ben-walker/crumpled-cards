3822b9c75394575769c31f681060c071536716e1
Ben Walker <bwalke05@gmail.com>
2018-11-05 14:53:56 -0500

    Simplify local login strategy

--- coffee-casino/src/config/passport/strategies/local.js
+++ coffee-casino/src/config/passport/strategies/local.js
@@ -1,4 +1,5 @@
 import local from 'passport-local';
+import to from 'await-to-js';
 import { User } from '../../../models';
 
 const LocalStrategy = local.Strategy;
@@ -6,18 +7,16 @@ const LocalStrategy = local.Strategy;
 const localLogin = new LocalStrategy({
   usernameField: 'identifier',
 },
-((identifier, password, done) => {
-  User.findOne({
+(async (identifier, password, done) => {
+  const [err, user] = await to(User.findOne({
     $or: [
       { username: identifier },
       { email: identifier.toLocaleLowerCase() },
     ],
-  }, (err, user) => {
-    if (err) return done(err);
-    if (!user) return done(null, false);
-    if (!user.comparePassword(password)) return done(null, false);
-    return done(null, user);
-  });
+  }));
+  if (err) return done(err);
+  if (!user || !user.comparePassword(password)) return done(null, false);
+  return done(null, user);
 }));
 
 export default localLogin;
