74f184769ddd29f3a3a848f310b09016dbb8836a
Ben Walker <bwalke05@gmail.com>
2018-12-12 15:16:46 -0500

    Remove unused profile route

--- vue-poker/src/config/router/routes/index.js
+++ vue-poker/src/config/router/routes/index.js
@@ -2,12 +2,10 @@ import home from './home'
 import welcome from './welcome'
 import redirect from './redirect'
 import authenticate from './authenticate'
-import profile from './profile'
 
 export default [
   home,
   welcome,
   redirect,
-  authenticate,
-  profile
+  authenticate
 ]
