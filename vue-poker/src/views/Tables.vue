07148d522336abe81dfa748d5722f0526565b549
Ben Walker <bwalke05@gmail.com>
2019-01-03 20:24:13 -0500

    Add some mock tables to the Tables screen

--- vue-poker/src/views/Tables.vue
+++ vue-poker/src/views/Tables.vue
@@ -3,6 +3,12 @@
     .section
       .container
         br
+        .columns.is-multiline
+          .column.is-half(v-for="table in tables")
+            .box
+              p.title.has-text-grey-dark.is-unselectable.is-marginless {{ table.title }}
+              p.help.is-unselectable {{ table.time }}
+              .has-text-right
     fab(
       :actions="[]"
       :enable-rotation="false"
@@ -19,6 +25,15 @@ import fab from 'vue-fab'
 
 export default {
   name: 'tables',
+  data () {
+    return {
+      tables: [
+        { title: 'Mock Table 1', time: 'Created 4 hours ago' },
+        { title: 'Mock Table 2', time: 'Created < an hour ago' },
+        { title: 'Mock Table 3', time: 'Created an hour ago' }
+      ]
+    }
+  },
   components: {
     TableModal,
     fab
