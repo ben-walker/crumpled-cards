f34cb313636036b79393d64cbb791dcf10472b01
Ben Walker <bwalke05@gmail.com>
2019-01-04 17:24:13 -0500

    Show time created on table tile

--- vue-poker/src/components/TableTile/tableTile.vue
+++ vue-poker/src/components/TableTile/tableTile.vue
@@ -2,16 +2,25 @@
   .table-tile
     .box
       p.title.has-text-grey-dark.is-unselectable.is-marginless {{ table.title }}
+      p.help.is-unselectable {{ createdAtReadable }}
       .has-text-right
 </template>
 
 <script>
+import moment from 'moment'
+
 export default {
   name: 'tableTile',
   props: {
     table: {
       type: Object,
       required: true
+    }
+  },
+  computed: {
+    createdAtReadable () {
+      const diff = moment().to(this.table.createdAt)
+      return `Created ${diff}`
     }
   }
 }
