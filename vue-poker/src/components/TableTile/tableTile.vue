9d9a8fdf6349f150941d615a7aea451b718cdc83
Ben Walker <bwalke05@gmail.com>
2019-01-09 19:53:07 -0500

    Remove 'join table' button

--- vue-poker/src/components/TableTile/tableTile.vue
+++ vue-poker/src/components/TableTile/tableTile.vue
@@ -4,9 +4,6 @@
       p.title.has-text-grey-dark.is-unselectable.is-marginless {{ table.title }}
       p.help.is-unselectable {{ createdAtReadable }}
       .has-text-right
-        button.button.is-primary
-          b-icon(pack="fas" icon="sign-in-alt" size="is-small")
-          span Join Table
 </template>
 
 <script>
